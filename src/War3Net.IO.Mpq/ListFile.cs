51c02e1ff00ad02308787f27baf33b2aa071a71d
Drake53 <49623303+Drake53@users.noreply.github.com>
2019-06-28 18:03:35 +0200

    Code cleanup for ListFile.cs

--- src/War3Net.IO.Mpq/ListFile.cs
+++ src/War3Net.IO.Mpq/ListFile.cs
@@ -1,57 +1,93 @@
-﻿using System;
+﻿// ------------------------------------------------------------------------------
+// <copyright file="ListFile.cs" company="Foole (fooleau@gmail.com)">
+// Copyright (c) 2006 Foole (fooleau@gmail.com). All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// ------------------------------------------------------------------------------
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
 
 namespace Foole.Mpq
 {
-    public class ListFile
+    /// <summary>
+    /// The <see cref="ListFile"/> lists all files that are contained in the <see cref="MpqArchive"/>.
+    /// </summary>
+    public sealed class ListFile : IDisposable
     {
+        /// <summary>
+        /// The key (filename) used to open the <see cref="ListFile"/>.
+        /// </summary>
         public const string Key = "(listfile)";
 
-        private Stream _baseStream;
+        private readonly Stream _baseStream;
         private bool _readOnly;
 
-        public ListFile( IEnumerable<string> files )
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ListFile"/> class.
+        /// </summary>
+        /// <param name="files">The collection of file paths to be included in the <see cref="ListFile"/>.</param>
+        /// <exception cref="ArgumentNullException">Thrown when the <paramref name="files"/> argument is null.</exception>
+        public ListFile(IEnumerable<string> files)
         {
             _baseStream = new MemoryStream();
             _readOnly = false;
 
-            using ( var writer = GetWriter() )
+            using (var writer = GetWriter())
             {
-                foreach ( var fileName in files )
+                foreach (var fileName in files ?? throw new ArgumentNullException(nameof(files)))
                 {
-                    writer.WriteLine( fileName );
+                    writer.WriteLine(fileName);
                 }
             }
 
             _baseStream.Position = 0;
         }
 
+        /// <summary>
+        /// Gets the underlying stream of this <see cref="ListFile"/>.
+        /// </summary>
         public Stream BaseStream => _baseStream;
 
-        public void WriteFile( string fileName )
+        /// <summary>
+        /// Appends a single file path to the <see cref="ListFile"/>.
+        /// </summary>
+        /// <param name="fileName">The file path to append.</param>
+        public void WriteFile(string fileName)
         {
-            using ( var writer = GetWriter() )
+            using (var writer = GetWriter())
             {
-                writer.WriteLine( fileName );
+                writer.WriteLine(fileName);
             }
         }
 
+        /// <summary>
+        /// Make the <see cref="ListFile"/> read-only.
+        /// </summary>
         public void Finish()
         {
             _baseStream.Position = 0;
             _readOnly = true;
         }
 
+        /// <summary>
+        /// Releases all resources used by the <see cref="ListFile"/>.
+        /// </summary>
+        public void Dispose()
+        {
+            _baseStream.Dispose();
+        }
+
         private StreamWriter GetWriter()
         {
-            if ( _readOnly )
+            if (_readOnly)
             {
-                throw new IOException();
+                throw new InvalidOperationException($"Cannot obtain a {nameof(StreamWriter)} for this {nameof(ListFile)}, because it's read-only.");
             }
 
-            return new StreamWriter( _baseStream, new UTF8Encoding( false, true ), 1024, true );
+            return new StreamWriter(_baseStream, new UTF8Encoding(false, true), 1024, true);
         }
     }
 }
