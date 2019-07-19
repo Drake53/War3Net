0b647c7471eab52e0445bceb72803a94c22f137c
Drake53 <49623303+Drake53@users.noreply.github.com>
2019-07-19 20:23:51 +0200

    Add some basic tests for mpq library.

---
+++ src/War3Net.IO.Mpq.Tests/MpqArchiveTest.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace War3Net.IO.Mpq.Tests
+{
+    [TestClass]
+    public class BlpImageTest
+    {
+        [DataTestMethod]
+        [DataRow("TestData/Lorem Ipsum.txt", (ushort)8)]
+        public void TestStoreThenRetrieveFile(string filename, ushort blockSize)
+        {
+            var fileStream = File.OpenRead(filename);
+            var mpqFile = new MpqFile(fileStream, filename, MpqFileFlags.Exists, blockSize);
+            var archive = MpqArchive.Create(new MemoryStream(), new List<MpqFile>() { mpqFile });
+
+            var openedArchive = MpqArchive.Open(archive.BaseStream);
+            var openedStream = openedArchive.OpenFile(filename);
+
+            // Reload file, since the filestream gets disposed when the mpqfile is added to an mpqarchive.
+            fileStream = File.OpenRead(filename);
+
+            Assert.AreEqual(fileStream.Length, openedStream.Length);
+
+            using (var fileStreamReader = new StreamReader(fileStream))
+            {
+                using (var openedStreamReader = new StreamReader(openedStream))
+                {
+                    StringAssert.Equals(fileStreamReader.ReadToEnd(), openedStreamReader.ReadToEnd());
+                }
+            }
+        }
+
+        [DataTestMethod]
+        [DataRow("TestData/Lorem Ipsum.txt", (ushort)8)]
+        public void TestStoreCompressedThenRetrieveFile(string filename, ushort blockSize)
+        {
+            var fileStream = File.OpenRead(filename);
+            var mpqFile = new MpqFile(fileStream, filename, MpqFileFlags.Exists | MpqFileFlags.Compressed, blockSize);
+            var archive = MpqArchive.Create(new MemoryStream(), new List<MpqFile>() { mpqFile });
+
+            var openedArchive = MpqArchive.Open(archive.BaseStream);
+            var openedStream = openedArchive.OpenFile(filename);
+
+            // Reload file, since the filestream gets disposed when the mpqfile is added to an mpqarchive.
+            fileStream = File.OpenRead(filename);
+
+            Assert.AreEqual(fileStream.Length, openedStream.Length);
+
+            using (var fileStreamReader = new StreamReader(fileStream))
+            {
+                using (var openedStreamReader = new StreamReader(openedStream))
+                {
+                    StringAssert.Equals(fileStreamReader.ReadToEnd(), openedStreamReader.ReadToEnd());
+                }
+            }
+        }
+    }
+}
---
+++ src/War3Net.IO.Mpq/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// [assembly: AssemblyTitle("War3Net.Drawing.Tga")]
+// [assembly: AssemblyConfiguration("")]
+// [assembly: AssemblyCompany("shns")]
+// [assembly: AssemblyProduct("War3Net.Drawing.Tga")]
+//[assembly: AssemblyDescription("Library to decode TARGA image format.")]
+//[assembly: AssemblyCopyright("Copyright © 2016 shns")]
+//[assembly: AssemblyTrademark("")]
+//[assembly: AssemblyCulture("")]
+
+[assembly: InternalsVisibleTo("War3Net.IO.Mpq.Tests")]
