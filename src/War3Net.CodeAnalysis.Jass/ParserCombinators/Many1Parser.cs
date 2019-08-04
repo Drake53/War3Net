fccb534a1d4e705f27749b706a37394a13e8b4c9
Drake53 <49623303+Drake53@users.noreply.github.com>
2019-08-04 16:22:01 +0200

    Minor speed optimization to Many1Parser by using greedy algorithm.

--- src/War3Net.CodeAnalysis.Jass/ParserCombinators/Many1Parser.cs
+++ src/War3Net.CodeAnalysis.Jass/ParserCombinators/Many1Parser.cs
@@ -5,6 +5,10 @@
 // </copyright>
 // ------------------------------------------------------------------------------
 
+// #define MANY1_PARSE_FRINGE_FIFO
+// #define MANY1_PARSE_NO_YIELD
+#define MANY1_PARSE_GREEDY
+
 using System;
 using System.Collections.Generic;
 
@@ -37,15 +41,59 @@ namespace War3Net.CodeAnalysis.Jass
             }
         }
 
+#if MANY1_PARSE_GREEDY
+        public IEnumerable<ParseResult> Parse(ParseState state)
+        {
+            foreach (var (nodes, consumedTokens) in Parse(state, new List<SyntaxNode>(), 0))
+            {
+                yield return new ParseResult(CreateNode(nodes), consumedTokens);
+            }
+        }
+
+        private IEnumerable<(List<SyntaxNode> nodes, int consumedTokens)> Parse(ParseState state, List<SyntaxNode> currentNodes, int consumedTokens)
+        {
+            foreach (var result in _baseParser.Parse(state))
+            {
+                var totalTokensConsumed = result.ConsumedTokens + consumedTokens;
+                var newState = new ParseState();
+                newState.Position = state.Position + result.ConsumedTokens;
+                newState.Tokens = state.Tokens;
+
+                var newNodes = new List<SyntaxNode>(currentNodes);
+                newNodes.Add(result.Node);
+
+                foreach (var newResult in Parse(newState, newNodes, totalTokensConsumed))
+                {
+                    yield return newResult;
+                }
+
+                yield return (new List<SyntaxNode>(newNodes), totalTokensConsumed);
+            }
+        }
+#else
         public IEnumerable<ParseResult> Parse(ParseState state)
         {
             var startPosition = state.Position;
-            var stack = new Stack<(SyntaxNode[] nodes, ParseState state)>();
-            stack.Push((Array.Empty<SyntaxNode>(), state));
 
-            while (stack.Count > 0)
+#if MANY1_PARSE_FRINGE_FIFO
+            var fringe = new Queue<(SyntaxNode[] nodes, ParseState state)>();
+            fringe.Enqueue((Array.Empty<SyntaxNode>(), state));
+#else
+            var fringe = new Stack<(SyntaxNode[] nodes, ParseState state)>();
+            fringe.Push((Array.Empty<SyntaxNode>(), state));
+#endif
+
+#if MANY1_PARSE_NO_YIELD
+            var results = new Stack<ParseResult>();
+#endif
+
+            while (fringe.Count > 0)
             {
-                var next = stack.Pop();
+#if MANY1_PARSE_FRINGE_FIFO
+                var next = fringe.Dequeue();
+#else
+                var next = fringe.Pop();
+#endif
 
                 foreach (var subResult in _baseParser.Parse(next.state))
                 {
@@ -57,16 +105,37 @@ namespace War3Net.CodeAnalysis.Jass
                     next.nodes.CopyTo(newNodes, 0);
                     newNodes[newNodes.Length - 1] = subResult.Node;
 
+#if MANY1_PARSE_NO_YIELD
+                    results.Push(new ParseResult(CreateNode(newNodes), newState.Position - startPosition));
+#else
                     yield return new ParseResult(CreateNode(newNodes), newState.Position - startPosition);
+#endif
 
-                    stack.Push((newNodes, newState));
+#if MANY1_PARSE_FRINGE_FIFO
+                    fringe.Enqueue((newNodes, newState));
+#else
+                    fringe.Push((newNodes, newState));
+#endif
                 }
             }
+
+#if MANY1_PARSE_NO_YIELD
+            while (results.Count > 0)
+            {
+                yield return results.Pop();
+            }
+#endif
         }
+#endif
 
         protected virtual SyntaxNode CreateNode(params SyntaxNode[] nodes)
         {
             return new UnnamedNode(nodes);
+        }
+
+        private SyntaxNode CreateNode(List<SyntaxNode> nodes)
+        {
+            return CreateNode(nodes.ToArray());
         }
     }
 }
