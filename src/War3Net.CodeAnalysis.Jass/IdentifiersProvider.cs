438476711a09f7e4a1f5521e62446323efe88954
Drake53 <49623303+Drake53@users.noreply.github.com>
2019-09-02 20:39:59 +0200

    Add source code obfuscator for jass.

---
+++ src/War3Net.CodeAnalysis.Jass/IdentifiersProvider.cs
@@ -0,0 +1,46 @@
+﻿// ------------------------------------------------------------------------------
+// <copyright file="IdentifiersProvider.cs" company="Drake53">
+// Copyright (c) 2019 Drake53. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// ------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+
+namespace War3Net.CodeAnalysis.Jass
+{
+    public static class IdentifiersProvider
+    {
+        public static IEnumerable<string> GetIdentifiers(string filePath)
+        {
+            var fileSyntax = JassParser.ParseFile( filePath );
+
+            foreach (var declarationNode in fileSyntax.DeclarationList)
+            {
+                var declr = declarationNode.Declaration;
+                if (declr.GlobalsBlock != null)
+                {
+                    foreach (var globalDeclaration in declr.GlobalsBlock.GlobalDeclarationListNode)
+                    {
+                        yield return globalDeclaration.ConstantDeclarationNode?.IdentifierNameNode.ValueText
+                                  ?? globalDeclaration.VariableDeclarationNode.DeclarationNode.VariableDefinitionNode?.IdentifierNameNode.ValueText
+                                  ?? globalDeclaration.VariableDeclarationNode.DeclarationNode.ArrayDefinitionNode.IdentifierNameNode.ValueText;
+                    }
+                }
+                else if (declr.TypeDefinition != null)
+                {
+                    yield return declr.TypeDefinition.NewTypeNameNode.ValueText;
+                }
+                else
+                {
+                    yield return declr.NativeFunctionDeclaration.FunctionDeclarationNode.IdentifierNode.ValueText;
+                }
+            }
+
+            foreach (var functionNode in fileSyntax.FunctionList)
+            {
+                yield return functionNode.FunctionDeclarationNode.IdentifierNode.ValueText;
+            }
+        }
+    }
+}
---
+++ src/War3Net.CodeAnalysis.Jass/JassObfuscator.cs
@@ -0,0 +1,72 @@
+﻿// ------------------------------------------------------------------------------
+// <copyright file="JassObfuscator.cs" company="Drake53">
+// Copyright (c) 2019 Drake53. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// ------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+using War3Net.CodeAnalysis.Jass.Renderer;
+
+namespace War3Net.CodeAnalysis.Jass
+{
+    public static class JassObfuscator
+    {
+        public static void Obfuscate(string inputFile, string outputFile, params string[] referenceFiles)
+        {
+            var fileSyntax = JassParser.ParseFile(inputFile);
+
+            File.Delete(outputFile);
+            using (var fileStream = File.OpenWrite(outputFile))
+            {
+                using (var streamWriter = new StreamWriter(fileStream, new UTF8Encoding(false, true)))
+                {
+                    var renderer = new JassRenderer(streamWriter);
+                    renderer.SetNewlineString(true, false);
+                    renderer.Comments = false;
+                    renderer.Indentation = 0;
+                    renderer.OptionalWhitespace = false;
+                    renderer.OmitEmptyLines = true;
+                    renderer.InlineConstants = true;
+
+                    var renameDictionary = new Dictionary<string, string>();
+                    var exceptions = new HashSet<string>();
+                    exceptions.Add("main");
+                    exceptions.Add("config");
+
+                    foreach (var referenceFile in referenceFiles)
+                    {
+                        foreach (var identifier in IdentifiersProvider.GetIdentifiers(referenceFile))
+                        {
+                            exceptions.Add(identifier);
+                        }
+                    }
+
+                    renderer.SetIdentifierOptimizerMethod(
+                        (s) =>
+                        {
+                            // Make exceptions for init stuff, since these are called using ExecuteFunc
+                            if (exceptions.Contains(s) || s.StartsWith("jasshelper__initstructs") || s.EndsWith("__onInit"))
+                            {
+                                return s;
+                            }
+
+                            if (!renameDictionary.ContainsKey(s))
+                            {
+                                var renamed = $"j_{renameDictionary.Count}";
+                                renameDictionary.Add(s, renamed);
+                                return renamed;
+                            }
+
+                            return renameDictionary[s];
+                        });
+
+                    renderer.Render(fileSyntax);
+                }
+            }
+        }
+    }
+}
