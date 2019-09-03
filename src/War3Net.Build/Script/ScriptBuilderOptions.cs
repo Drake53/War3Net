7e57def984b89651ca5549eb8526dcc43c1b7f10
Drake53 <49623303+Drake53@users.noreply.github.com>
2019-09-03 13:41:30 +0200

    Add options objects for scriptbuilder and -compiler.

---
+++ src/War3Net.Build/Script/ScriptBuilderOptions.cs
@@ -0,0 +1,55 @@
+﻿// ------------------------------------------------------------------------------
+// <copyright file="ScriptBuilderOptions.cs" company="Drake53">
+// Copyright (c) 2019 Drake53. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// ------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+
+namespace War3Net.Build.Script
+{
+    public sealed class ScriptBuilderOptions
+    {
+        public float CameraBoundsLeft { get; set; }
+
+        public float CameraBoundsRight { get; set; }
+
+        public float CameraBoundsTop { get; set; }
+
+        public float CameraBoundsBottom { get; set; }
+
+        public LightEnvironment LightEnvironment { get; set; }
+
+        public SoundEnvironment SoundEnvironment { get; set; }
+
+        public string MapName { get; set; }
+
+        public string MapDescription { get; set; }
+
+        public string LobbyMusic { get; set; }
+
+        public List<string> InitializationFunctions { get; private set; }
+
+        public List<PlayerSlot> PlayerSlots { get; private set; }
+
+        public ScriptBuilderOptions()
+        {
+            CameraBoundsLeft = -999999f;
+            CameraBoundsRight = 999999f;
+            CameraBoundsTop = 999999f;
+            CameraBoundsBottom = -999999f;
+
+            LightEnvironment = LightEnvironment.LordaeronSummer;
+            SoundEnvironment = SoundEnvironment.Cityscape;
+
+            MapName = "Just Another Warcraft III Map";
+            MapDescription = "Generated with War3Net.Build";
+
+            LobbyMusic = null;
+
+            InitializationFunctions = new List<string>();
+            PlayerSlots = new List<PlayerSlot>();
+        }
+    }
+}
---
+++ src/War3Net.Build/Script/ScriptCompilerOptions.cs
@@ -0,0 +1,24 @@
+﻿// ------------------------------------------------------------------------------
+// <copyright file="ScriptCompilerOptions.cs" company="Drake53">
+// Copyright (c) 2019 Drake53. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// ------------------------------------------------------------------------------
+
+namespace War3Net.Build.Script
+{
+    public sealed class ScriptCompilerOptions
+    {
+        public string SourceDirectory { get; set; }
+
+        public string OutputDirectory { get; set; }
+
+        public bool Debug { get; set; }
+
+        public bool Optimize { get; set; }
+
+        public bool Obfuscate { get; set; }
+
+        public ScriptBuilderOptions BuilderOptions { get; set; }
+    }
+}
