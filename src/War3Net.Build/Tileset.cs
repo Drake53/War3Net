c0738412607940f6e4cc7d8d18f5d8eff919272d
Drake53 <49623303+Drake53@users.noreply.github.com>
2019-09-16 13:17:51 +0200

    Rename file.

--- src/War3Net.Build/Tileset.cs
+++ src/War3Net.Build/Tileset.cs
@@ -1,5 +1,5 @@
 ﻿// ------------------------------------------------------------------------------
-// <copyright file="LightEnvironment.cs" company="Drake53">
+// <copyright file="Tileset.cs" company="Drake53">
 // Copyright (c) 2019 Drake53. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 // </copyright>
