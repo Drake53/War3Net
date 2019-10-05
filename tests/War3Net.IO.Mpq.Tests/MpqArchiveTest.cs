23d17c51f5928e9b9b5c19ef38fe4cd834783122
Drake53 <49623303+Drake53@users.noreply.github.com>
2019-10-05 16:09:54 +0200

    Add tests for War3Net.IO.Mpq project.

---
+++ tests/War3Net.IO.Mpq.Tests/MpqArchiveTest.cs
@@ -0,0 +1,100 @@
+﻿// ------------------------------------------------------------------------------
+// <copyright file="MpqArchiveTest.cs" company="Drake53">
+// Licensed under the MIT license.
+// See the LICENSE file in the project root for more information.
+// </copyright>
+// ------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace War3Net.IO.Mpq.Tests
+{
+    [TestClass]
+    public class MpqArchiveTest
+    {
+        private const ushort BlockSize = 3;
+
+        [DataTestMethod]
+        [DynamicData(nameof(GetTestFiles), DynamicDataSourceType.Method)]
+        public void TestStoreThenRetrieveFile(string filename)
+        {
+            var fileStream = File.OpenRead(filename);
+            var mpqFile = new MpqFile(fileStream, filename, MpqLocale.Neutral, MpqFileFlags.Exists, BlockSize);
+            var archive = MpqArchive.Create(new MemoryStream(), new List<MpqFile>() { mpqFile });
+
+            var openedArchive = MpqArchive.Open(archive.BaseStream);
+            var openedStream = openedArchive.OpenFile(filename);
+
+            // Reload file, since the filestream gets disposed when the mpqfile is added to an mpqarchive.
+            fileStream = File.OpenRead(filename);
+
+            StreamAssert.AreEqual(fileStream, openedStream);
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(GetTestFiles), DynamicDataSourceType.Method)]
+        public void TestStoreCompressedThenRetrieveFile(string filename)
+        {
+            TestStoreThenRetrieveFile(filename, MpqFileFlags.Exists | MpqFileFlags.Compressed);
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(GetTestFiles), DynamicDataSourceType.Method)]
+        public void TestStoreCompressedSingleUnitThenRetrieveFile(string filename)
+        {
+            TestStoreThenRetrieveFile(filename, MpqFileFlags.Exists | MpqFileFlags.Compressed | MpqFileFlags.SingleUnit);
+        }
+
+        private void TestStoreThenRetrieveFile(string filename, MpqFileFlags flags)
+        {
+            var fileStream = File.OpenRead(filename);
+            var mpqFile = new MpqFile(fileStream, filename, MpqLocale.Neutral, flags, BlockSize);
+            var archive = MpqArchive.Create(new MemoryStream(), new List<MpqFile>() { mpqFile }, blockSize: BlockSize);
+
+            var openedArchive = MpqArchive.Open(archive.BaseStream);
+            var openedStream = openedArchive.OpenFile(filename);
+
+            // Reload file, since the filestream gets disposed when the mpqfile is added to an mpqarchive.
+            fileStream = File.OpenRead(filename);
+
+            StreamAssert.AreEqual(fileStream, openedStream);
+        }
+
+        [TestMethod]
+        public void TestRecreatePKCompressed()
+        {
+            const string inputArchivePath = @"TestArchives\PKCompressed.w3x";
+
+            using var inputArchive = MpqArchive.Open(inputArchivePath);
+            using var recreatedArchive = MpqArchive.Create(
+                (Stream?)null,
+                inputArchive.GetMpqFiles().ToArray(),
+                (ushort)inputArchive.Header.HashTableSize,
+                inputArchive.Header.BlockSize);
+
+            for (var i = 0; i < inputArchive.Header.BlockTableSize; i++)
+            {
+                inputArchive.BaseStream.Position = inputArchive[i].FilePosition!.Value;
+                recreatedArchive.BaseStream.Position = recreatedArchive[i].FilePosition!.Value;
+
+                var size1 = inputArchive[i].CompressedSize;
+                var size2 = recreatedArchive[i].CompressedSize;
+                StreamAssert.AreEqual(inputArchive.BaseStream, recreatedArchive.BaseStream, size1 > size2 ? size1 : size2);
+            }
+
+            StreamAssert.AreEqual(inputArchive.BaseStream, recreatedArchive.BaseStream);
+        }
+
+        private static IEnumerable<object[]> GetTestFiles()
+        {
+            foreach (var file in Directory.EnumerateFiles("TestData", "*", SearchOption.TopDirectoryOnly))
+            {
+                yield return new object[] { file };
+            }
+        }
+    }
+}
---
+++ tests/War3Net.IO.Mpq.Tests/StreamAssert.cs
@@ -0,0 +1,101 @@
+﻿// ------------------------------------------------------------------------------
+// <copyright file="StreamAssert.cs" company="Drake53">
+// Licensed under the MIT license.
+// See the LICENSE file in the project root for more information.
+// </copyright>
+// ------------------------------------------------------------------------------
+
+using System;
+using System.IO;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace War3Net.IO.Mpq.Tests
+{
+    internal static class StreamAssert
+    {
+        internal static void AreEqual(Stream s1, Stream s2)
+        {
+            Assert.IsTrue(AreStreamsEqual(s1, s2));
+        }
+
+        internal static void AreEqual(Stream s1, Stream s2, long lengthToCheck)
+        {
+            Assert.IsTrue(AreStreamsEqual(s1, s2, lengthToCheck, out var message), message);
+        }
+
+        private static bool AreStreamsEqual(Stream s1, Stream s2)
+        {
+            if (s1.Length != s2.Length)
+            {
+                return false;
+            }
+
+            s1.Position = 0;
+            s2.Position = 0;
+
+            while (true)
+            {
+                var s1read = s1.ReadByte();
+                var s2read = s2.ReadByte();
+
+                if (s1read != s2read)
+                {
+                    return false;
+                }
+
+                if (s1read == -1)
+                {
+                    return true;
+                }
+            }
+        }
+
+        private static bool AreStreamsEqual(Stream s1, Stream s2, long lengthToCheck, out string message)
+        {
+            var result = true;
+            message = "\r\n";
+
+            var lengthRemaining1 = s1.Length - s1.Position;
+            var lengthRemaining2 = s2.Length - s2.Position;
+
+            if (lengthRemaining1 == lengthRemaining2)
+            {
+                if (lengthToCheck > lengthRemaining1)
+                {
+                    message += $"[Warning]: Checking {lengthToCheck} bytes in streams, when they only have {lengthRemaining1} bytes remaining to be read.\r\n";
+                }
+            }
+            else
+            {
+                if (lengthToCheck - lengthRemaining1 > 0 || lengthToCheck - lengthRemaining2 > 0)
+                {
+                    message += $"[Error]: Streams have different length.\r\n";
+                    result = false;
+                }
+            }
+
+            for (var bytesRead = 0; bytesRead < lengthToCheck; bytesRead++)
+            {
+                var s1read = s1.ReadByte();
+                var s2read = s2.ReadByte();
+
+                if (s1read == s2read)
+                {
+                    Console.WriteLine($"{bytesRead}: {s1read}");
+                }
+                else
+                {
+                    Console.WriteLine($"{bytesRead}: {s1read} != {s2read}");
+                    if (result)
+                    {
+                        result = false;
+                        message += $"[Error]: First mismatch at byte {bytesRead}";
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
