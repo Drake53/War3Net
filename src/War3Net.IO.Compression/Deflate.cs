bd91343211492e0068d29a4793f113192a60ff50
Drake53 <49623303+Drake53@users.noreply.github.com>
2019-10-15 23:24:25 +0200

    Clean up deprecated code from inflate and deflate methods.

--- src/War3Net.IO.Compression/Deflate.cs
+++ src/War3Net.IO.Compression/Deflate.cs
@@ -1,27 +1,19 @@
-﻿// #define SYSTEM_DEFLATE
-// #define SYSTEM_INFLATE
+﻿// ------------------------------------------------------------------------------
+// <copyright file="Deflate.cs" company="Drake53">
+// Licensed under the MIT license.
+// See the LICENSE file in the project root for more information.
+// </copyright>
+// ------------------------------------------------------------------------------
 
-#define WITH_PKLIB_HEADER
-#define LEAVE_UNCOMPRESSED_IF_LARGER
-
+#define LEAVE_UNCOMPRESSED_IF_LARGER // TODO: depends on whether or not file has SingleUnit flag?
 #define USING_DOTNETZIP
-
-#if USING_DOTNETZIP
-#undef WITH_PKLIB_HEADER
-// #undef LEAVE_UNCOMPRESSED_IF_LARGER
-#endif
 
 using System;
 using System.IO;
 
-#if SYSTEM_DEFLATE || SYSTEM_INFLATE
-using System.IO.Compression;
-#endif
-
 #if USING_DOTNETZIP
-#endif
-
-#if (!SYSTEM_DEFLATE || !SYSTEM_INFLATE) //&& !USING_DOTNETZIP
+using Ionic.Zlib;
+#else
 using ICSharpCode.SharpZipLib.Zip.Compression.Streams;
 #endif
 
@@ -34,25 +26,13 @@ namespace War3Net.IO.Compression
             _ = inputStream ?? throw new ArgumentNullException(nameof(inputStream));
             _ = outputStream ?? throw new ArgumentNullException(nameof(outputStream));
 
-#if WITH_PKLIB_HEADER
-            var position = outputStream.Position;
-            for (var i = 0; i < 8; i++)
-            {
-                outputStream.WriteByte(0);
-            }
-#endif
-
             var offset = inputStream.Position;
             var compressed = new MemoryStream();
             CompressTo(inputStream, compressed, (int)bytes, true);
 
 #if LEAVE_UNCOMPRESSED_IF_LARGER
-#if USING_DOTNETZIP
             // Add one because CompressionType byte not written yet.
             if ((compressed.Length + 1) >= bytes)
-#else
-            if (compressed.Length >= bytes)
-#endif
             {
                 compressed.Dispose();
 
@@ -71,9 +51,7 @@ namespace War3Net.IO.Compression
             else
 #endif
             {
-#if USING_DOTNETZIP
                 outputStream.WriteByte((byte)CompressionType.ZLib);
-#endif
                 compressed.Position = 0;
                 compressed.CopyTo(outputStream);
                 compressed.Dispose();
@@ -85,21 +63,6 @@ namespace War3Net.IO.Compression
             }
 
             var result = (uint)outputStream.Position;
-#if WITH_PKLIB_HEADER
-            var compressedSize = result - (uint)position;
-
-            outputStream.Position = position;
-            outputStream.WriteByte((byte)CompressionType.PKLib);
-            outputStream.WriteByte(0);
-            outputStream.WriteByte(0);
-            outputStream.WriteByte(0);
-            outputStream.WriteByte((byte)(compressedSize & 0xff));
-            outputStream.WriteByte((byte)((compressedSize >> 8) & 0xff));
-            outputStream.WriteByte((byte)((compressedSize >> 16) & 0xff));
-            outputStream.WriteByte((byte)((compressedSize >> 24) & 0xff));
-
-            outputStream.Position = result;
-#endif
             return result;
         }
 
@@ -109,7 +72,7 @@ namespace War3Net.IO.Compression
             _ = outputStream ?? throw new ArgumentNullException(nameof(outputStream));
 
 #if USING_DOTNETZIP
-            using (var deflater = new Ionic.Zlib.ZlibStream(outputStream, Ionic.Zlib.CompressionMode.Compress, true))
+            using (var deflater = new ZlibStream(outputStream, CompressionMode.Compress, true))
             {
                 for (var i = 0; i < bytes; i++)
                 {
@@ -121,62 +84,11 @@ namespace War3Net.IO.Compression
 
                     deflater.WriteByte((byte)r);
                 }
-
-                // deflater.FlushMode = Ionic.Zlib.FlushType.Finish;
-                // deflater.Flush();
             }
-#else
-            outputStream.WriteByte((byte)CompressionType.ZLib);
-#if SYSTEM_DEFLATE
-            const CompressionLevel compressionLevel = CompressionLevel.Optimal;
-
-            // TODO: don't hardcode header bytes
-            outputStream.WriteByte(0x78);
-            outputStream.WriteByte(compressionLevel == CompressionLevel.Optimal ? (byte)0xDA : throw new NotSupportedException());
-
-            const int mod = 65521;
-            var s1 = 1;
-            var s2 = 0;
-
-            using (var deflater = new DeflateStream(outputStream, compressionLevel, true))
-            {
-                for (var i = 0; i < bytes; i++)
-                {
-                    var r = inputStream.ReadByte();
-                    if (r == -1)
-                    {
-                        break;
-                    }
-
-                    deflater.WriteByte((byte)r);
-
-                    s1 += r;
-                    if (s1 >= mod)
-                    {
-                        s1 -= mod;
-                    }
-
-                    s2 += s1;
-                    if (s2 >= mod)
-                    {
-                        s2 -= mod;
-                    }
-                }
-            }
-
-            // Adler-32 checksum
-            outputStream.WriteByte((byte)((s2 >> 8) & 0xff));
-            outputStream.WriteByte((byte)(s2 & 0xff));
-            outputStream.WriteByte((byte)((s1 >> 8) & 0xff));
-            outputStream.WriteByte((byte)(s1 & 0xff));
 #else
             using (var stream = new MemoryStream())
             {
-#if SYSTEM_DEFLATE
-                using (var deflater = new DeflateStream(stream, CompressionMode.Compress))
-#else
                 using (var deflater = new DeflaterOutputStream(stream))
-#endif
                 {
                     for (var i = 0; i < bytes; i++)
                     {
@@ -189,16 +101,13 @@ namespace War3Net.IO.Compression
                         deflater.WriteByte((byte)r);
                     }
 
-#if !SYSTEM_DEFLATE
                     deflater.Finish();
-#endif
                     deflater.Flush();
 
                     stream.Position = 0;
                     stream.CopyTo(outputStream);
                 }
             }
-#endif
 #endif
 
             if (!leaveOpen)
@@ -213,12 +122,7 @@ namespace War3Net.IO.Compression
         {
             var output = new byte[expectedLength];
 #if USING_DOTNETZIP
-            using var inflater = new Ionic.Zlib.ZlibStream(compressedData, Ionic.Zlib.CompressionMode.Decompress, true);
-#elif SYSTEM_INFLATE
-            _ = compressedData.ReadByte() == 0x78 ? (object?)null : throw new InvalidDataException();
-            var b2 = compressedData.ReadByte();
-            _ = b2 == 0xDA || b2 == 0x9C ? (object?)null : throw new InvalidDataException();
-            using var inflater = new DeflateStream(compressedData, CompressionMode.Decompress, true);
+            using var inflater = new ZlibStream(compressedData, CompressionMode.Decompress, true);
 #else
             var inflater = new InflaterInputStream(compressedData);
 #endif
@@ -234,34 +138,6 @@ namespace War3Net.IO.Compression
                 offset += size;
                 expectedLength -= size;
             }
-
-#if SYSTEM_INFLATE
-            // Verify Adler-32 checksum.
-            const int mod = 65521;
-            var s1 = 1;
-            var s2 = 0;
-
-            foreach (var b in output)
-            {
-                s1 += b;
-                if (s1 >= mod)
-                {
-                    s1 -= mod;
-                }
-
-                s2 += s1;
-                if (s2 >= mod)
-                {
-                    s2 -= mod;
-                }
-            }
-
-            compressedData.Seek(-4, SeekOrigin.End);
-            _ = compressedData.ReadByte() == ((s2 >> 8) & 0xff) ? (object?)null : throw new InvalidDataException();
-            _ = compressedData.ReadByte() == (s2 & 0xff) ? (object?)null : throw new InvalidDataException();
-            _ = compressedData.ReadByte() == ((s1 >> 8) & 0xff) ? (object?)null : throw new InvalidDataException();
-            _ = compressedData.ReadByte() == (s1 & 0xff) ? (object?)null : throw new InvalidDataException();
-#endif
 
             return output;
         }
