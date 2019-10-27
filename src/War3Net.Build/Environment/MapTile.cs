722353b03fd77f0a54ed1c02021066099ef886fa
Drake53 <49623303+Drake53@users.noreply.github.com>
2019-10-27 20:45:53 +0100

    Implement MapTile object for .w3e files.

---
+++ src/War3Net.Build/Environment/MapTile.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace War3Net.Build.Environment
+{
+    public sealed class MapTile
+    {
+        public const int TileWidth = 128;
+        public const int TileHeight = 128;
+
+        /// <summary>
+        /// Size in bytes.
+        /// </summary>
+        public const int Size = 7;
+
+        private ushort _heightData;
+        private ushort _waterDataAndEdgeFlag;
+        private byte _textureDataAndFlags;
+        private byte _variationData;
+        private byte _cliffData;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MapTile"/> class.
+        /// </summary>
+        public MapTile(bool isEdgeTile)
+        {
+            IsEdgeTile = isEdgeTile;
+        }
+
+        private MapTile()
+        {
+        }
+
+        public float Height
+        {
+            get => (_heightData - 8192f) / 512f;
+            set => _heightData = (ushort)((value * 512f) + 8192f);
+        }
+
+        public float WaterHeight
+        {
+            get => ((_waterDataAndEdgeFlag & 0x3FFF) - 8192f) / 512f;
+            set => _waterDataAndEdgeFlag = (ushort)(((int)((value * 512f) + 8192f) & 0x3FFF) | (_waterDataAndEdgeFlag & 0x4000));
+        }
+
+        public bool IsEdgeTile
+        {
+            get => (_waterDataAndEdgeFlag & 0x4000) != 0;
+            set => _waterDataAndEdgeFlag = (ushort)(value ? _waterDataAndEdgeFlag | 0x4000 : _waterDataAndEdgeFlag & 0x3FFF);
+        }
+
+        public int Texture
+        {
+            get => _textureDataAndFlags & 0x0F;
+            set => _textureDataAndFlags = (value >= 0 && value <= 0x0F) ? (byte)(value | (_textureDataAndFlags & 0xF0)) : throw new ArgumentOutOfRangeException(nameof(value));
+        }
+
+        public bool IsRamp
+        {
+            get => (_textureDataAndFlags & 0x10) != 0;
+            set => _textureDataAndFlags = (byte)(value ? _textureDataAndFlags | 0x10 : _textureDataAndFlags & 0xEF);
+        }
+
+        public bool IsBlighted
+        {
+            get => (_textureDataAndFlags & 0x20) != 0;
+            set => _textureDataAndFlags = (byte)(value ? _textureDataAndFlags | 0x20 : _textureDataAndFlags & 0xDF);
+        }
+
+        public bool IsWater
+        {
+            get => (_textureDataAndFlags & 0x40) != 0;
+            set => _textureDataAndFlags = (byte)(value ? _textureDataAndFlags | 0x40 : _textureDataAndFlags & 0xBF);
+        }
+
+        public bool IsBoundary
+        {
+            get => (_textureDataAndFlags & 0x80) != 0;
+            set => _textureDataAndFlags = (byte)(value ? _textureDataAndFlags | 0x80 : _textureDataAndFlags & 0x7F);
+        }
+
+        public int Variation
+        {
+            get => _variationData & 0x0F;
+            set => _variationData = (value >= 0 && value <= 0x0F) ? (byte)(value | (_variationData & 0xF0)) : throw new ArgumentOutOfRangeException(nameof(value));
+        }
+
+        public int CliffVariation
+        {
+            get => (_variationData & 0xF0) >> 4;
+            set => _variationData = (value >= 0 && value <= 0x0F) ? (byte)((value << 4) | (_variationData & 0x0F)) : throw new ArgumentOutOfRangeException(nameof(value));
+        }
+
+        public int CliffLevel
+        {
+            get => _cliffData & 0x0F;
+            set => _cliffData = (value >= 0 && value <= 0x0F) ? (byte)(value | (_cliffData & 0xF0)) : throw new ArgumentOutOfRangeException(nameof(value));
+        }
+
+        public int CliffTexture
+        {
+            get => (_cliffData & 0xF0) >> 4;
+            set => _cliffData = (value >= 0 && value <= 0x0F) ? (byte)((value << 4) | (_cliffData & 0x0F)) : throw new ArgumentOutOfRangeException(nameof(value));
+        }
+
+        public static MapTile Parse(Stream stream, bool leaveOpen = false)
+        {
+            var tile = new MapTile();
+            using (var reader = new BinaryReader(stream, new UTF8Encoding(false, true), leaveOpen))
+            {
+                tile._heightData = reader.ReadUInt16();
+                tile._waterDataAndEdgeFlag = reader.ReadUInt16();
+                tile._textureDataAndFlags = reader.ReadByte();
+                tile._variationData = reader.ReadByte();
+                tile._cliffData = reader.ReadByte();
+            }
+
+            return tile;
+        }
+
+        public void WriteTo(BinaryWriter writer)
+        {
+            writer.Write(_heightData);
+            writer.Write(_waterDataAndEdgeFlag);
+            writer.Write(_textureDataAndFlags);
+            writer.Write(_variationData);
+            writer.Write(_cliffData);
+        }
+    }
+}
