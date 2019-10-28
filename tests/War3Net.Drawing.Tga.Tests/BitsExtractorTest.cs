48aa9af0a0641b201935ac29ebf1f38bc5622d2d
Drake53 <49623303+Drake53@users.noreply.github.com>
2019-10-28 16:46:57 +0100

    Add TGA tests.

---
+++ tests/War3Net.Drawing.Tga.Tests/BitsExtractorTest.cs
@@ -0,0 +1,30 @@
+﻿// ------------------------------------------------------------------------------
+// <copyright file="BitsExtractorTest.cs" company="Drake53">
+// Licensed under the MIT license.
+// See the LICENSE file in the project root for more information.
+// </copyright>
+// ------------------------------------------------------------------------------
+
+using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace War3Net.Drawing.Tga.Tests
+{
+    [TestClass]
+    public class BitsExtractorTest
+    {
+        [TestMethod]
+        public void TestExtractByte()
+        {
+            Assert.AreEqual(19, BitsExtractor.Extract(0xCC, 2, 5));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestExtractByteException()
+        {
+            _ = BitsExtractor.Extract(0xCC, 2, 7);
+        }
+    }
+}
