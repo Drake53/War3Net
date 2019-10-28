6f22f184d8134e3d0ec5c3ce2055f82fdb011db6
Drake53 <49623303+Drake53@users.noreply.github.com>
2019-10-28 16:06:42 +0100

    Add test for IsDefaultTileset method using default .w3e files.

---
+++ tests/War3Net.Build.Tests/MapEnvironmentTest.cs
@@ -0,0 +1,37 @@
+﻿// ------------------------------------------------------------------------------
+// <copyright file="MapEnvironmentTest.cs" company="Drake53">
+// Licensed under the MIT license.
+// See the LICENSE file in the project root for more information.
+// </copyright>
+// ------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.IO;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using War3Net.Build.Environment;
+
+namespace War3Net.Build.Tests
+{
+    [TestClass]
+    public class MapEnvironmentTest
+    {
+        [DataTestMethod]
+        [DynamicData(nameof(GetDefaultEnvironmentFiles), DynamicDataSourceType.Method)]
+        public void TestDefaultTileset(string environmentFilePath)
+        {
+            using var fileStream = File.OpenRead(environmentFilePath);
+            var environment = MapEnvironment.Parse(fileStream);
+            Assert.IsTrue(environment.IsDefaultTileset());
+        }
+
+        private static IEnumerable<object[]> GetDefaultEnvironmentFiles()
+        {
+            foreach (var env in Directory.EnumerateFiles(@".\TestData\Environment\Default"))
+            {
+                yield return new[] { env };
+            }
+        }
+    }
+}
