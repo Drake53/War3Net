220a035a027add04e9a10b056ac0a386adceeb13
Drake53 <49623303+Drake53@users.noreply.github.com>
2019-10-29 16:53:39 +0100

    Add extension method.

---
+++ src/War3Net.IO.Mpq/StreamExtensions.cs
@@ -0,0 +1,31 @@
+﻿// ------------------------------------------------------------------------------
+// <copyright file="StreamExtensions.cs" company="Drake53">
+// Licensed under the MIT license.
+// See the LICENSE file in the project root for more information.
+// </copyright>
+// ------------------------------------------------------------------------------
+
+using System.IO;
+
+namespace War3Net.IO.Mpq
+{
+    public static class StreamExtensions
+    {
+        private const int _DefaultCopyBufferSize = 81920;
+
+        public static void CopyTo(this Stream stream, Stream destination, int bytesToCopy, int bufferSize = _DefaultCopyBufferSize)
+        {
+            var buffer = new byte[bufferSize];
+            while (bytesToCopy > 0)
+            {
+                var toRead = bytesToCopy > bufferSize ? bufferSize : bytesToCopy;
+                var read = stream.Read(buffer, 0, toRead);
+                if (read != 0)
+                {
+                    destination.Write(buffer, 0, read);
+                    bytesToCopy -= read;
+                }
+            }
+        }
+    }
+}
