bde624c87a12fad2c7875799b444f948eb5e24be
Drake53 <49623303+Drake53@users.noreply.github.com>
2019-11-10 16:10:37 +0100

    Add test data.

---
+++ tests/War3Net.Build.Tests/TestData/Script/Template/Program.cs
@@ -0,0 +1,12 @@
+﻿using static War3Api.Common;
+
+namespace War3Net.Build.Tests.TestData.Script.Template
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            DisplayTextToPlayer(GetLocalPlayer(), 0, 0, "Hello World!");
+        }
+    }
+}
