84e2375e087fc3e5536525676a035bca5e9e39de
Drake53 <49623303+Drake53@users.noreply.github.com>
2019-12-29 19:14:13 +0100

    Add tests to parse .doo files.

---
+++ tests/War3Net.Build.Tests/MapWidgetsTest.cs
@@ -0,0 +1,69 @@
+﻿// ------------------------------------------------------------------------------
+// <copyright file="MapWidgetsTest.cs" company="Drake53">
+// Licensed under the MIT license.
+// See the LICENSE file in the project root for more information.
+// </copyright>
+// ------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.IO;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using War3Net.Build.Widget;
+using War3Net.Common.Testing;
+
+namespace War3Net.Build.Tests
+{
+    [TestClass]
+    public class MapWidgetsTest
+    {
+        [DataTestMethod]
+        [DynamicData(nameof(GetMapDoodadsData), DynamicDataSourceType.Method)]
+        public void TestParseMapDoodads(string mapDoodadsFilePath)
+        {
+            using var recreated = new MemoryStream();
+            using var original = File.OpenRead(mapDoodadsFilePath);
+
+            var data = MapDoodads.Parse(original, true);
+            data.SerializeTo(recreated, true);
+
+            // TODO: skip header, since it's ignored in SerializeTo method (so always saves as TFT version)
+            //if (data.)
+
+            StreamAssert.AreEqual(original, recreated, true);
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(GetMapUnitsData), DynamicDataSourceType.Method)]
+        public void TestParseMapUnits(string mapUnitsFilePath)
+        {
+            using var recreated = new MemoryStream();
+            using var original = File.OpenRead(mapUnitsFilePath);
+
+            var data = MapUnits.Parse(original, true);
+            data.SerializeTo(recreated, true);
+
+            // TODO: skip header, since it's ignored in SerializeTo method (so always saves as TFT version)
+            //if (data.)
+
+            StreamAssert.AreEqual(original, recreated, true);
+        }
+
+        private static IEnumerable<object[]> GetMapDoodadsData()
+        {
+            foreach (var unitData in Directory.EnumerateFiles(@".\TestData\Widget\Doodads"))
+            {
+                yield return new[] { unitData };
+            }
+        }
+
+        private static IEnumerable<object[]> GetMapUnitsData()
+        {
+            foreach (var unitData in Directory.EnumerateFiles(@".\TestData\Widget\Units"))
+            {
+                yield return new[] { unitData };
+            }
+        }
+    }
+}
