3f6e1e6ba5f1576b8f8338e98eee30d0e9abc6b4
Drake53 <49623303+Drake53@users.noreply.github.com>
2020-01-06 22:29:44 +0100

    Parse war3map.w3r files.

---
+++ src/War3Net.Build/Environment/MapRegions.cs
@@ -0,0 +1,75 @@
+﻿// ------------------------------------------------------------------------------
+// <copyright file="MapRegions.cs" company="Drake53">
+// Licensed under the MIT license.
+// See the LICENSE file in the project root for more information.
+// </copyright>
+// ------------------------------------------------------------------------------
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace War3Net.Build.Environment
+{
+    public sealed class MapRegions : IEnumerable<Region>
+    {
+        public const string FileName = "war3map.w3r";
+        public const uint LatestVersion = 5;
+
+        private readonly List<Region> _regions;
+
+        private uint _version;
+
+        private MapRegions()
+        {
+            _regions = new List<Region>();
+        }
+
+        public static MapRegions Parse(Stream stream, bool leaveOpen = false)
+        {
+            var mapRegions = new MapRegions();
+            using (var reader = new BinaryReader(stream, new UTF8Encoding(false, true), leaveOpen))
+            {
+                mapRegions._version = reader.ReadUInt32();
+                if (mapRegions._version != LatestVersion)
+                {
+                    throw new Exception();
+                }
+
+                var regionCount = reader.ReadUInt32();
+                for (var i = 0; i < regionCount; i++)
+                {
+                    mapRegions._regions.Add(Region.Parse(stream, true));
+                }
+            }
+
+            return mapRegions;
+        }
+
+        public void SerializeTo(Stream stream, bool leaveOpen = false)
+        {
+            using (var writer = new BinaryWriter(stream, new UTF8Encoding(false, true), leaveOpen))
+            {
+                writer.Write(LatestVersion);
+
+                writer.Write(_regions.Count);
+                foreach (var region in _regions)
+                {
+                    region.WriteTo(writer);
+                }
+            }
+        }
+
+        public IEnumerator<Region> GetEnumerator()
+        {
+            return ((IEnumerable<Region>)_regions).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable<Region>)_regions).GetEnumerator();
+        }
+    }
+}
---
+++ src/War3Net.Build/Environment/Region.cs
@@ -0,0 +1,80 @@
+﻿// ------------------------------------------------------------------------------
+// <copyright file="Region.cs" company="Drake53">
+// Licensed under the MIT license.
+// See the LICENSE file in the project root for more information.
+// </copyright>
+// ------------------------------------------------------------------------------
+
+using System.Drawing;
+using System.IO;
+using System.Text;
+
+using War3Net.Common.Extensions;
+
+namespace War3Net.Build.Environment
+{
+    public sealed class Region
+    {
+        private float _left;
+        private float _bottom;
+        private float _right;
+        private float _top;
+
+        private string _name;
+        private int _creationNumber;
+
+        private char[] _weatherId;
+        private string _ambientSound;
+
+        private Color _color;
+
+        public static Region Parse(Stream stream, bool leaveOpen)
+        {
+            var region = new Region();
+            using (var reader = new BinaryReader(stream, new UTF8Encoding(false, true), leaveOpen))
+            {
+                region._left = reader.ReadSingle();
+                region._bottom = reader.ReadSingle();
+                region._right = reader.ReadSingle();
+                region._top = reader.ReadSingle();
+
+                region._name = reader.ReadChars();
+                region._creationNumber = reader.ReadInt32();
+
+                region._weatherId = reader.ReadChars(4);
+                region._ambientSound = reader.ReadChars();
+
+                region._color = Color.FromArgb(reader.ReadInt32());
+            }
+
+            return region;
+        }
+
+        public void SerializeTo(Stream stream, bool leaveOpen = false)
+        {
+            using (var writer = new BinaryWriter(stream, new UTF8Encoding(false, true), leaveOpen))
+            {
+                WriteTo(writer);
+            }
+        }
+
+        public void WriteTo(BinaryWriter writer)
+        {
+            writer.Write(_left);
+            writer.Write(_bottom);
+            writer.Write(_right);
+            writer.Write(_top);
+
+            writer.WriteString(_name);
+            writer.Write(_creationNumber);
+
+            writer.Write(_weatherId);
+            writer.WriteString(_ambientSound);
+
+            writer.Write(_color.B);
+            writer.Write(_color.G);
+            writer.Write(_color.R);
+            writer.Write(_color.A);
+        }
+    }
+}
