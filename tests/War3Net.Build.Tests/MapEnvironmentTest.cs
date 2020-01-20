6893b65cb6ad4c42c87089e92d74a58cf300268c
Drake53 <49623303+Drake53@users.noreply.github.com>
2020-01-20 11:11:00 +0100

    Add tests.

--- tests/War3Net.Build.Tests/MapEnvironmentTest.cs
+++ tests/War3Net.Build.Tests/MapEnvironmentTest.cs
@@ -11,6 +11,7 @@ using System.IO;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using War3Net.Build.Environment;
+using War3Net.Common.Testing;
 
 namespace War3Net.Build.Tests
 {
@@ -26,11 +27,47 @@ namespace War3Net.Build.Tests
             Assert.IsTrue(environment.IsDefaultTileset());
         }
 
+        [DataTestMethod]
+        [DynamicData(nameof(GetDefaultPathingFiles), DynamicDataSourceType.Method)]
+        public void TestPathingMap(string pathingMapFile)
+        {
+            using var fileStream = File.OpenRead(pathingMapFile);
+            var pathingMap = PathingMap.Parse(fileStream, true);
+            using var memoryStream = new MemoryStream();
+            pathingMap.SerializeTo(memoryStream, true);
+
+            StreamAssert.AreEqual(fileStream, memoryStream, true);
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(GetDefaultRegionFiles), DynamicDataSourceType.Method)]
+        public void TestMapRegions(string regionsFilePath)
+        {
+            using var fileStream = File.OpenRead(regionsFilePath);
+            var mapRegions = MapRegions.Parse(fileStream);
+        }
+
         private static IEnumerable<object[]> GetDefaultEnvironmentFiles()
         {
             foreach (var env in Directory.EnumerateFiles(@".\TestData\Environment\Default"))
             {
                 yield return new[] { env };
+            }
+        }
+
+        private static IEnumerable<object[]> GetDefaultPathingFiles()
+        {
+            foreach (var pathingMapFile in Directory.EnumerateFiles(@".\TestData\Pathing"))
+            {
+                yield return new[] { pathingMapFile };
+            }
+        }
+
+        private static IEnumerable<object[]> GetDefaultRegionFiles()
+        {
+            foreach (var mapRegions in Directory.EnumerateFiles(@".\TestData\Region"))
+            {
+                yield return new[] { mapRegions };
             }
         }
     }
