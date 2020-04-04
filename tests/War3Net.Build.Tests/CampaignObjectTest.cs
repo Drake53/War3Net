183d037cf4866721e773f1334ae431455c7255a0
Drake53 <49623303+Drake53@users.noreply.github.com>
2020-04-04 16:13:02 +0200

    Object data tests.

---
+++ tests/War3Net.Build.Tests/CampaignObjectTest.cs
@@ -0,0 +1,179 @@
+﻿// ------------------------------------------------------------------------------
+// <copyright file="CampaignObjectTest.cs" company="Drake53">
+// Licensed under the MIT license.
+// See the LICENSE file in the project root for more information.
+// </copyright>
+// ------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.IO;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using War3Net.Build.Object;
+using War3Net.Build.Providers;
+using War3Net.Common.Testing;
+
+namespace War3Net.Build.Tests
+{
+    [TestClass]
+    public class CampaignObjectTest
+    {
+        [DataTestMethod]
+        [DynamicData(nameof(GetCampaignUnitObjectData), DynamicDataSourceType.Method)]
+        public void TestParseCampaignUnitObjectData(string campaignUnitObjectDataFilePath)
+        {
+            using var original = FileProvider.GetFile(campaignUnitObjectDataFilePath);
+            using var recreated = new MemoryStream();
+
+            CampaignUnitObjectData.Parse(original, true).SerializeTo(recreated, true);
+            StreamAssert.AreEqual(original, recreated, true);
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(GetCampaignItemObjectData), DynamicDataSourceType.Method)]
+        public void TestParseCampaignItemObjectData(string campaignItemObjectDataFilePath)
+        {
+            using var original = FileProvider.GetFile(campaignItemObjectDataFilePath);
+            using var recreated = new MemoryStream();
+
+            CampaignItemObjectData.Parse(original, true).SerializeTo(recreated, true);
+            StreamAssert.AreEqual(original, recreated, true);
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(GetCampaignDestructableObjectData), DynamicDataSourceType.Method)]
+        public void TestParseCampaignDestructableObjectData(string campaignDestructableObjectDataFilePath)
+        {
+            using var original = FileProvider.GetFile(campaignDestructableObjectDataFilePath);
+            using var recreated = new MemoryStream();
+
+            CampaignDestructableObjectData.Parse(original, true).SerializeTo(recreated, true);
+            StreamAssert.AreEqual(original, recreated, true);
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(GetCampaignDoodadObjectData), DynamicDataSourceType.Method)]
+        public void TestParseCampaignDoodadObjectData(string campaignDoodadObjectDataFilePath)
+        {
+            using var original = FileProvider.GetFile(campaignDoodadObjectDataFilePath);
+            using var recreated = new MemoryStream();
+
+            CampaignDoodadObjectData.Parse(original, true).SerializeTo(recreated, true);
+            StreamAssert.AreEqual(original, recreated, true);
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(GetCampaignAbilityObjectData), DynamicDataSourceType.Method)]
+        public void TestParseCampaignAbilityObjectData(string campaignAbilityObjectDataFilePath)
+        {
+            using var original = FileProvider.GetFile(campaignAbilityObjectDataFilePath);
+            using var recreated = new MemoryStream();
+
+            CampaignAbilityObjectData.Parse(original, true).SerializeTo(recreated, true);
+            StreamAssert.AreEqual(original, recreated, true);
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(GetCampaignBuffObjectData), DynamicDataSourceType.Method)]
+        public void TestParseCampaignBuffObjectData(string campaignBuffObjectDataFilePath)
+        {
+            using var original = FileProvider.GetFile(campaignBuffObjectDataFilePath);
+            using var recreated = new MemoryStream();
+
+            CampaignBuffObjectData.Parse(original, true).SerializeTo(recreated, true);
+            StreamAssert.AreEqual(original, recreated, true);
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(GetCampaignUpgradeObjectData), DynamicDataSourceType.Method)]
+        public void TestParseCampaignUpgradeObjectData(string campaignUpgradeObjectDataFilePath)
+        {
+            using var original = FileProvider.GetFile(campaignUpgradeObjectDataFilePath);
+            using var recreated = new MemoryStream();
+
+            CampaignUpgradeObjectData.Parse(original, true).SerializeTo(recreated, true);
+            StreamAssert.AreEqual(original, recreated, true);
+        }
+
+        private static IEnumerable<object[]> GetCampaignUnitObjectData()
+        {
+            return GetCampaignObjectData("Unit", CampaignUnitObjectData.FileName);
+        }
+
+        private static IEnumerable<object[]> GetCampaignItemObjectData()
+        {
+            return GetCampaignObjectData("Item", CampaignItemObjectData.FileName);
+        }
+
+        private static IEnumerable<object[]> GetCampaignDestructableObjectData()
+        {
+            return GetCampaignObjectData("Destructable", CampaignDestructableObjectData.FileName);
+        }
+
+        private static IEnumerable<object[]> GetCampaignDoodadObjectData()
+        {
+            return GetCampaignObjectData("Doodad", CampaignDoodadObjectData.FileName);
+        }
+
+        private static IEnumerable<object[]> GetCampaignAbilityObjectData()
+        {
+            return GetCampaignObjectData("Ability", CampaignAbilityObjectData.FileName);
+        }
+
+        private static IEnumerable<object[]> GetCampaignBuffObjectData()
+        {
+            return GetCampaignObjectData("Buff", CampaignBuffObjectData.FileName);
+        }
+
+        private static IEnumerable<object[]> GetCampaignUpgradeObjectData()
+        {
+            return GetCampaignObjectData("Upgrade", CampaignUpgradeObjectData.FileName);
+        }
+
+        private static IEnumerable<object[]> GetCampaignObjectData(string directory, string fileName)
+        {
+            var pattern = $"*{new FileInfo(fileName).Extension}";
+
+            if (Directory.Exists($@".\TestData\Object\{directory}"))
+            {
+                foreach (var file in Directory.EnumerateFiles($@".\TestData\Object\{directory}", pattern, SearchOption.AllDirectories))
+                {
+                    yield return new[] { file };
+                }
+            }
+
+            if (Directory.Exists($@".\TestData\Local\Object\{directory}"))
+            {
+                foreach (var file in Directory.EnumerateFiles($@".\TestData\Local\Object\{directory}", pattern, SearchOption.AllDirectories))
+                {
+                    yield return new[] { file };
+                }
+            }
+
+            if (Directory.Exists(@".\TestData\Campaigns"))
+            {
+                foreach (var campaignPath in Directory.EnumerateFiles(@".\TestData\Campaigns", "*.w3n", SearchOption.TopDirectoryOnly))
+                {
+                    var file = Path.Combine(campaignPath, fileName);
+                    if (FileProvider.FileExists(file))
+                    {
+                        yield return new[] { file };
+                    }
+                }
+            }
+
+            if (Directory.Exists(@".\TestData\Local\Campaigns"))
+            {
+                foreach (var campaignPath in Directory.EnumerateFiles(@".\TestData\Local\Campaigns", "*.w3n", SearchOption.TopDirectoryOnly))
+                {
+                    var file = Path.Combine(campaignPath, fileName);
+                    if (FileProvider.FileExists(file))
+                    {
+                        yield return new[] { file };
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ tests/War3Net.Build.Tests/MapObjectTest.cs
@@ -0,0 +1,197 @@
+﻿// ------------------------------------------------------------------------------
+// <copyright file="MapObjectTest.cs" company="Drake53">
+// Licensed under the MIT license.
+// See the LICENSE file in the project root for more information.
+// </copyright>
+// ------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.IO;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using War3Net.Build.Object;
+using War3Net.Build.Providers;
+using War3Net.Common.Testing;
+
+namespace War3Net.Build.Tests
+{
+    [TestClass]
+    public class MapObjectTest
+    {
+        [DataTestMethod]
+        [DynamicData(nameof(GetMapUnitObjectData), DynamicDataSourceType.Method)]
+        public void TestParseMapUnitObjectData(string mapUnitObjectDataFilePath)
+        {
+            using var original = FileProvider.GetFile(mapUnitObjectDataFilePath);
+            using var recreated = new MemoryStream();
+
+            MapUnitObjectData.Parse(original, true).SerializeTo(recreated, true);
+            StreamAssert.AreEqual(original, recreated, true);
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(GetMapItemObjectData), DynamicDataSourceType.Method)]
+        public void TestParseMapItemObjectData(string mapItemObjectDataFilePath)
+        {
+            using var original = FileProvider.GetFile(mapItemObjectDataFilePath);
+            using var recreated = new MemoryStream();
+
+            MapItemObjectData.Parse(original, true).SerializeTo(recreated, true);
+            StreamAssert.AreEqual(original, recreated, true);
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(GetMapDestructableObjectData), DynamicDataSourceType.Method)]
+        public void TestParseMapDestructableObjectData(string mapDestructableObjectDataFilePath)
+        {
+            using var original = FileProvider.GetFile(mapDestructableObjectDataFilePath);
+            using var recreated = new MemoryStream();
+
+            MapDestructableObjectData.Parse(original, true).SerializeTo(recreated, true);
+            StreamAssert.AreEqual(original, recreated, true);
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(GetMapDoodadObjectData), DynamicDataSourceType.Method)]
+        public void TestParseMapDoodadObjectData(string mapDoodadObjectDataFilePath)
+        {
+            using var original = FileProvider.GetFile(mapDoodadObjectDataFilePath);
+            using var recreated = new MemoryStream();
+
+            MapDoodadObjectData.Parse(original, true).SerializeTo(recreated, true);
+            StreamAssert.AreEqual(original, recreated, true);
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(GetMapAbilityObjectData), DynamicDataSourceType.Method)]
+        public void TestParseMapAbilityObjectData(string mapAbilityObjectDataFilePath)
+        {
+            using var original = FileProvider.GetFile(mapAbilityObjectDataFilePath);
+            using var recreated = new MemoryStream();
+
+            MapAbilityObjectData.Parse(original, true).SerializeTo(recreated, true);
+            StreamAssert.AreEqual(original, recreated, true);
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(GetMapBuffObjectData), DynamicDataSourceType.Method)]
+        public void TestParseMapBuffObjectData(string mapBuffObjectDataFilePath)
+        {
+            using var original = FileProvider.GetFile(mapBuffObjectDataFilePath);
+            using var recreated = new MemoryStream();
+
+            MapBuffObjectData.Parse(original, true).SerializeTo(recreated, true);
+            StreamAssert.AreEqual(original, recreated, true);
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(GetMapUpgradeObjectData), DynamicDataSourceType.Method)]
+        public void TestParseMapUpgradeObjectData(string mapUpgradeObjectDataFilePath)
+        {
+            using var original = FileProvider.GetFile(mapUpgradeObjectDataFilePath);
+            using var recreated = new MemoryStream();
+
+            MapUpgradeObjectData.Parse(original, true).SerializeTo(recreated, true);
+            StreamAssert.AreEqual(original, recreated, true);
+        }
+
+        private static IEnumerable<object[]> GetMapUnitObjectData()
+        {
+            return GetMapObjectData("Unit", MapUnitObjectData.FileName);
+        }
+
+        private static IEnumerable<object[]> GetMapItemObjectData()
+        {
+            return GetMapObjectData("Item", MapItemObjectData.FileName);
+        }
+
+        private static IEnumerable<object[]> GetMapDestructableObjectData()
+        {
+            return GetMapObjectData("Destructable", MapDestructableObjectData.FileName);
+        }
+
+        private static IEnumerable<object[]> GetMapDoodadObjectData()
+        {
+            return GetMapObjectData("Doodad", MapDoodadObjectData.FileName);
+        }
+
+        private static IEnumerable<object[]> GetMapAbilityObjectData()
+        {
+            return GetMapObjectData("Ability", MapAbilityObjectData.FileName);
+        }
+
+        private static IEnumerable<object[]> GetMapBuffObjectData()
+        {
+            return GetMapObjectData("Buff", MapBuffObjectData.FileName);
+        }
+
+        private static IEnumerable<object[]> GetMapUpgradeObjectData()
+        {
+            return GetMapObjectData("Upgrade", MapUpgradeObjectData.FileName);
+        }
+
+        private static IEnumerable<object[]> GetMapObjectData(string directory, string fileName)
+        {
+            var pattern = $"*{new FileInfo(fileName).Extension}";
+
+            if (Directory.Exists($@".\TestData\Object\{directory}"))
+            {
+                foreach (var file in Directory.EnumerateFiles($@".\TestData\Object\{directory}", pattern, SearchOption.AllDirectories))
+                {
+                    yield return new[] { file };
+                }
+            }
+
+            if (Directory.Exists($@".\TestData\Local\Object\{directory}"))
+            {
+                foreach (var file in Directory.EnumerateFiles($@".\TestData\Local\Object\{directory}", pattern, SearchOption.AllDirectories))
+                {
+                    yield return new[] { file };
+                }
+            }
+
+            if (Directory.Exists(@".\TestData\Maps"))
+            {
+                foreach (var mapPath in Directory.EnumerateFiles(@".\TestData\Maps", "*.w3m", SearchOption.TopDirectoryOnly))
+                {
+                    var file = Path.Combine(mapPath, fileName);
+                    if (FileProvider.FileExists(file))
+                    {
+                        yield return new[] { file };
+                    }
+                }
+
+                foreach (var mapPath in Directory.EnumerateFiles(@".\TestData\Maps", "*.w3x", SearchOption.TopDirectoryOnly))
+                {
+                    var file = Path.Combine(mapPath, fileName);
+                    if (FileProvider.FileExists(file))
+                    {
+                        yield return new[] { file };
+                    }
+                }
+            }
+
+            if (Directory.Exists(@".\TestData\Local\Maps"))
+            {
+                foreach (var mapPath in Directory.EnumerateFiles(@".\TestData\Local\Maps", "*.w3m", SearchOption.TopDirectoryOnly))
+                {
+                    var file = Path.Combine(mapPath, fileName);
+                    if (FileProvider.FileExists(file))
+                    {
+                        yield return new[] { file };
+                    }
+                }
+
+                foreach (var mapPath in Directory.EnumerateFiles(@".\TestData\Local\Maps", "*.w3x", SearchOption.TopDirectoryOnly))
+                {
+                    var file = Path.Combine(mapPath, fileName);
+                    if (FileProvider.FileExists(file))
+                    {
+                        yield return new[] { file };
+                    }
+                }
+            }
+        }
+    }
+}
