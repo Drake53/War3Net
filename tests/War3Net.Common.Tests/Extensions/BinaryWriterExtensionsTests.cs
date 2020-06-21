404c25a6cfaed60ef9914bceaf2c8754f55c9e33
Drake53 <49623303+Drake53@users.noreply.github.com>
2020-06-21 16:28:43 +0200

    Add TestData class.

--- tests/War3Net.Common.Tests/Extensions/BinaryWriterExtensionsTests.cs
+++ tests/War3Net.Common.Tests/Extensions/BinaryWriterExtensionsTests.cs
@@ -68,20 +68,7 @@ namespace War3Net.Common.Tests.Extensions
 
         private static IEnumerable<object?[]> GetTestWriteStrings()
         {
-            yield return new[] { "Hello world!" };
-            yield return new[] { string.Empty };
-            yield return new[] { (string?)null };
-
-            yield return new object?[] { "String cannot contain \0 characters.", typeof(ArgumentException) };
-            yield return new[] { "But it can if it's the last character: \0" };
-            yield return new[] { "\0" };
-
-            yield return new object?[] { "\uD83D", typeof(ArgumentException) };
-            yield return new object?[] { "\uD83D\uD83D", typeof(ArgumentException) };
-            yield return new object?[] { "\uDCAB", typeof(ArgumentException) };
-            yield return new object?[] { "\uDCAB\uDCAB", typeof(ArgumentException) };
-            yield return new object?[] { "\uDCAB\uD83D", typeof(ArgumentException) };
-            yield return new[] { "\uD83D\uDCAB" };
+            return TestData.GetTestStrings();
         }
     }
 }
---
+++ tests/War3Net.Common.Tests/TestData.cs
@@ -0,0 +1,35 @@
+﻿// ------------------------------------------------------------------------------
+// <copyright file="TestData.cs" company="Drake53">
+// Licensed under the MIT license.
+// See the LICENSE file in the project root for more information.
+// </copyright>
+// ------------------------------------------------------------------------------
+
+#nullable enable
+
+using System;
+using System.Collections.Generic;
+
+namespace War3Net.Common.Tests
+{
+    internal static class TestData
+    {
+        internal static IEnumerable<object?[]> GetTestStrings()
+        {
+            yield return new[] { "Hello world!" };
+            yield return new[] { string.Empty };
+            yield return new[] { (string?)null };
+
+            yield return new object?[] { "String cannot contain \0 characters.", typeof(ArgumentException) };
+            yield return new[] { "But it can if it's the last character: \0" };
+            yield return new[] { "\0" };
+
+            yield return new object?[] { "\uD83D", typeof(ArgumentException) };
+            yield return new object?[] { "\uD83D\uD83D", typeof(ArgumentException) };
+            yield return new object?[] { "\uDCAB", typeof(ArgumentException) };
+            yield return new object?[] { "\uDCAB\uDCAB", typeof(ArgumentException) };
+            yield return new object?[] { "\uDCAB\uD83D", typeof(ArgumentException) };
+            yield return new[] { "\uD83D\uDCAB" };
+        }
+    }
+}
