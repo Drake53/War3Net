5507c527a6fe8d067e721fd3804181f2c3e66ec6
Drake53 <49623303+Drake53@users.noreply.github.com>
2020-09-03 12:12:43 +0200

    Remove old test files.

--- tests/War3Net.Build.Tests/CampaignObjectTest.cs
+++ tests/War3Net.Build.Tests/CampaignObjectTest.cs
@@ -6,15 +6,11 @@
 // ------------------------------------------------------------------------------
 
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using War3Net.Build.Object;
-using War3Net.Build.Providers;
-using War3Net.Common.Testing;
 
 namespace War3Net.Build.Tests
 {
@@ -38,147 +34,6 @@ namespace War3Net.Build.Tests
 
             memoryStream.Position = 0;
             CampaignObjectData.Parse(memoryStream);
-        }
-
-        [DataTestMethod]
-        [DynamicData(nameof(GetCampaignUnitObjectData), DynamicDataSourceType.Method)]
-        public void TestParseCampaignUnitObjectData(string campaignUnitObjectDataFilePath)
-        {
-            using var original = FileProvider.GetFile(campaignUnitObjectDataFilePath);
-            using var recreated = new MemoryStream();
-
-            CampaignUnitObjectData.Parse(original, true).SerializeTo(recreated, true);
-            StreamAssert.AreEqual(original, recreated, true, true);
-        }
-
-        [DataTestMethod]
-        [DynamicData(nameof(GetCampaignItemObjectData), DynamicDataSourceType.Method)]
-        public void TestParseCampaignItemObjectData(string campaignItemObjectDataFilePath)
-        {
-            using var original = FileProvider.GetFile(campaignItemObjectDataFilePath);
-            using var recreated = new MemoryStream();
-
-            CampaignItemObjectData.Parse(original, true).SerializeTo(recreated, true);
-            StreamAssert.AreEqual(original, recreated, true, true);
-        }
-
-        [DataTestMethod]
-        [DynamicData(nameof(GetCampaignDestructableObjectData), DynamicDataSourceType.Method)]
-        public void TestParseCampaignDestructableObjectData(string campaignDestructableObjectDataFilePath)
-        {
-            using var original = FileProvider.GetFile(campaignDestructableObjectDataFilePath);
-            using var recreated = new MemoryStream();
-
-            CampaignDestructableObjectData.Parse(original, true).SerializeTo(recreated, true);
-            StreamAssert.AreEqual(original, recreated, true, true);
-        }
-
-        [DataTestMethod]
-        [DynamicData(nameof(GetCampaignDoodadObjectData), DynamicDataSourceType.Method)]
-        public void TestParseCampaignDoodadObjectData(string campaignDoodadObjectDataFilePath)
-        {
-            using var original = FileProvider.GetFile(campaignDoodadObjectDataFilePath);
-            using var recreated = new MemoryStream();
-
-            CampaignDoodadObjectData.Parse(original, true).SerializeTo(recreated, true);
-            StreamAssert.AreEqual(original, recreated, true, true);
-        }
-
-        [DataTestMethod]
-        [DynamicData(nameof(GetCampaignAbilityObjectData), DynamicDataSourceType.Method)]
-        public void TestParseCampaignAbilityObjectData(string campaignAbilityObjectDataFilePath)
-        {
-            using var original = FileProvider.GetFile(campaignAbilityObjectDataFilePath);
-            using var recreated = new MemoryStream();
-
-            CampaignAbilityObjectData.Parse(original, true).SerializeTo(recreated, true);
-            StreamAssert.AreEqual(original, recreated, true, true);
-        }
-
-        [DataTestMethod]
-        [DynamicData(nameof(GetCampaignBuffObjectData), DynamicDataSourceType.Method)]
-        public void TestParseCampaignBuffObjectData(string campaignBuffObjectDataFilePath)
-        {
-            using var original = FileProvider.GetFile(campaignBuffObjectDataFilePath);
-            using var recreated = new MemoryStream();
-
-            CampaignBuffObjectData.Parse(original, true).SerializeTo(recreated, true);
-            StreamAssert.AreEqual(original, recreated, true, true);
-        }
-
-        [DataTestMethod]
-        [DynamicData(nameof(GetCampaignUpgradeObjectData), DynamicDataSourceType.Method)]
-        public void TestParseCampaignUpgradeObjectData(string campaignUpgradeObjectDataFilePath)
-        {
-            using var original = FileProvider.GetFile(campaignUpgradeObjectDataFilePath);
-            using var recreated = new MemoryStream();
-
-            CampaignUpgradeObjectData.Parse(original, true).SerializeTo(recreated, true);
-            StreamAssert.AreEqual(original, recreated, true, true);
-        }
-
-        [DataTestMethod]
-        [DynamicData(nameof(GetCampaignAllObjectData), DynamicDataSourceType.Method)]
-        public void TestParseCampaignObjectData(string campaignObjectDataFilePath)
-        {
-            using var original = FileProvider.GetFile(campaignObjectDataFilePath);
-            using var recreated = new MemoryStream();
-
-            CampaignObjectData.Parse(original, true).SerializeTo(recreated, true);
-            StreamAssert.AreEqual(original, recreated, true, true);
-        }
-
-        private static IEnumerable<object[]> GetCampaignUnitObjectData()
-        {
-            return GetCampaignObjectData("Unit", CampaignUnitObjectData.FileName);
-        }
-
-        private static IEnumerable<object[]> GetCampaignItemObjectData()
-        {
-            return GetCampaignObjectData("Item", CampaignItemObjectData.FileName);
-        }
-
-        private static IEnumerable<object[]> GetCampaignDestructableObjectData()
-        {
-            return GetCampaignObjectData("Destructable", CampaignDestructableObjectData.FileName);
-        }
-
-        private static IEnumerable<object[]> GetCampaignDoodadObjectData()
-        {
-            return GetCampaignObjectData("Doodad", CampaignDoodadObjectData.FileName);
-        }
-
-        private static IEnumerable<object[]> GetCampaignAbilityObjectData()
-        {
-            return GetCampaignObjectData("Ability", CampaignAbilityObjectData.FileName);
-        }
-
-        private static IEnumerable<object[]> GetCampaignBuffObjectData()
-        {
-            return GetCampaignObjectData("Buff", CampaignBuffObjectData.FileName);
-        }
-
-        private static IEnumerable<object[]> GetCampaignUpgradeObjectData()
-        {
-            return GetCampaignObjectData("Upgrade", CampaignUpgradeObjectData.FileName);
-        }
-
-        private static IEnumerable<object[]> GetCampaignAllObjectData()
-        {
-            return GetCampaignObjectData("All", CampaignObjectData.FileName);
-        }
-
-        private static IEnumerable<object[]> GetCampaignObjectData(string directory, string fileName)
-        {
-            return TestDataProvider.GetDynamicData(
-                fileName.GetSearchPattern(),
-                SearchOption.AllDirectories,
-                Path.Combine("Object", directory))
-
-            .Concat(TestDataProvider.GetDynamicArchiveData(
-                fileName,
-                SearchOption.TopDirectoryOnly,
-                "Campaigns"));
         }
     }
 }
--- tests/War3Net.Build.Tests/MapObjectTest.cs
+++ tests/War3Net.Build.Tests/MapObjectTest.cs
@@ -6,15 +6,11 @@
 // ------------------------------------------------------------------------------
 
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using War3Net.Build.Object;
-using War3Net.Build.Providers;
-using War3Net.Common.Testing;
 
 namespace War3Net.Build.Tests
 {
@@ -38,147 +34,6 @@ namespace War3Net.Build.Tests
 
             memoryStream.Position = 0;
             MapObjectData.Parse(memoryStream);
-        }
-
-        [DataTestMethod]
-        [DynamicData(nameof(GetMapUnitObjectData), DynamicDataSourceType.Method)]
-        public void TestParseMapUnitObjectData(string mapUnitObjectDataFilePath)
-        {
-            using var original = FileProvider.GetFile(mapUnitObjectDataFilePath);
-            using var recreated = new MemoryStream();
-
-            MapUnitObjectData.Parse(original, true).SerializeTo(recreated, true);
-            StreamAssert.AreEqual(original, recreated, true, true);
-        }
-
-        [DataTestMethod]
-        [DynamicData(nameof(GetMapItemObjectData), DynamicDataSourceType.Method)]
-        public void TestParseMapItemObjectData(string mapItemObjectDataFilePath)
-        {
-            using var original = FileProvider.GetFile(mapItemObjectDataFilePath);
-            using var recreated = new MemoryStream();
-
-            MapItemObjectData.Parse(original, true).SerializeTo(recreated, true);
-            StreamAssert.AreEqual(original, recreated, true, true);
-        }
-
-        [DataTestMethod]
-        [DynamicData(nameof(GetMapDestructableObjectData), DynamicDataSourceType.Method)]
-        public void TestParseMapDestructableObjectData(string mapDestructableObjectDataFilePath)
-        {
-            using var original = FileProvider.GetFile(mapDestructableObjectDataFilePath);
-            using var recreated = new MemoryStream();
-
-            MapDestructableObjectData.Parse(original, true).SerializeTo(recreated, true);
-            StreamAssert.AreEqual(original, recreated, true, true);
-        }
-
-        [DataTestMethod]
-        [DynamicData(nameof(GetMapDoodadObjectData), DynamicDataSourceType.Method)]
-        public void TestParseMapDoodadObjectData(string mapDoodadObjectDataFilePath)
-        {
-            using var original = FileProvider.GetFile(mapDoodadObjectDataFilePath);
-            using var recreated = new MemoryStream();
-
-            MapDoodadObjectData.Parse(original, true).SerializeTo(recreated, true);
-            StreamAssert.AreEqual(original, recreated, true, true);
-        }
-
-        [DataTestMethod]
-        [DynamicData(nameof(GetMapAbilityObjectData), DynamicDataSourceType.Method)]
-        public void TestParseMapAbilityObjectData(string mapAbilityObjectDataFilePath)
-        {
-            using var original = FileProvider.GetFile(mapAbilityObjectDataFilePath);
-            using var recreated = new MemoryStream();
-
-            MapAbilityObjectData.Parse(original, true).SerializeTo(recreated, true);
-            StreamAssert.AreEqual(original, recreated, true, true);
-        }
-
-        [DataTestMethod]
-        [DynamicData(nameof(GetMapBuffObjectData), DynamicDataSourceType.Method)]
-        public void TestParseMapBuffObjectData(string mapBuffObjectDataFilePath)
-        {
-            using var original = FileProvider.GetFile(mapBuffObjectDataFilePath);
-            using var recreated = new MemoryStream();
-
-            MapBuffObjectData.Parse(original, true).SerializeTo(recreated, true);
-            StreamAssert.AreEqual(original, recreated, true, true);
-        }
-
-        [DataTestMethod]
-        [DynamicData(nameof(GetMapUpgradeObjectData), DynamicDataSourceType.Method)]
-        public void TestParseMapUpgradeObjectData(string mapUpgradeObjectDataFilePath)
-        {
-            using var original = FileProvider.GetFile(mapUpgradeObjectDataFilePath);
-            using var recreated = new MemoryStream();
-
-            MapUpgradeObjectData.Parse(original, true).SerializeTo(recreated, true);
-            StreamAssert.AreEqual(original, recreated, true, true);
-        }
-
-        [DataTestMethod]
-        [DynamicData(nameof(GetMapAllObjectData), DynamicDataSourceType.Method)]
-        public void TestParseMapObjectData(string mapObjectDataFilePath)
-        {
-            using var original = FileProvider.GetFile(mapObjectDataFilePath);
-            using var recreated = new MemoryStream();
-
-            MapObjectData.Parse(original, true).SerializeTo(recreated, true);
-            StreamAssert.AreEqual(original, recreated, true, true);
-        }
-
-        private static IEnumerable<object[]> GetMapUnitObjectData()
-        {
-            return GetMapObjectData("Unit", MapUnitObjectData.FileName);
-        }
-
-        private static IEnumerable<object[]> GetMapItemObjectData()
-        {
-            return GetMapObjectData("Item", MapItemObjectData.FileName);
-        }
-
-        private static IEnumerable<object[]> GetMapDestructableObjectData()
-        {
-            return GetMapObjectData("Destructable", MapDestructableObjectData.FileName);
-        }
-
-        private static IEnumerable<object[]> GetMapDoodadObjectData()
-        {
-            return GetMapObjectData("Doodad", MapDoodadObjectData.FileName);
-        }
-
-        private static IEnumerable<object[]> GetMapAbilityObjectData()
-        {
-            return GetMapObjectData("Ability", MapAbilityObjectData.FileName);
-        }
-
-        private static IEnumerable<object[]> GetMapBuffObjectData()
-        {
-            return GetMapObjectData("Buff", MapBuffObjectData.FileName);
-        }
-
-        private static IEnumerable<object[]> GetMapUpgradeObjectData()
-        {
-            return GetMapObjectData("Upgrade", MapUpgradeObjectData.FileName);
-        }
-
-        private static IEnumerable<object[]> GetMapAllObjectData()
-        {
-            return GetMapObjectData("All", MapObjectData.FileName);
-        }
-
-        private static IEnumerable<object[]> GetMapObjectData(string directory, string fileName)
-        {
-            return TestDataProvider.GetDynamicData(
-                fileName.GetSearchPattern(),
-                SearchOption.AllDirectories,
-                Path.Combine("Object", directory))
-
-            .Concat(TestDataProvider.GetDynamicArchiveData(
-                fileName,
-                SearchOption.TopDirectoryOnly,
-                "Maps"));
         }
     }
 }
