aa4b57220965668db4184fafec4a475b0204e9bc
Drake53 <49623303+Drake53@users.noreply.github.com>
2020-10-27 12:37:28 +0100

    Add WeatherType enum members.

--- src/War3Net.Build.Core/Info/WeatherType.cs
+++ src/War3Net.Build.Core/Info/WeatherType.cs
@@ -7,9 +7,30 @@
 
 namespace War3Net.Build
 {
+    // war3.w3mod/terrainart/weather.slk
     public enum WeatherType
     {
-        // TODO: find out types and values
         None = 0,
+        AshenvaleHeavyRain = ('R' << 0) | ('A' << 8) | ('h' << 16) | ('r' << 24),
+        AshenvaleLightRain = ('R' << 0) | ('A' << 8) | ('l' << 16) | ('r' << 24),
+        DalaranShield = ('M' << 0) | ('E' << 8) | ('d' << 16) | ('s' << 24),
+        DungeonHeavyBlueFog = ('F' << 0) | ('D' << 8) | ('b' << 16) | ('h' << 24),
+        DungeonBlueFog = ('F' << 0) | ('D' << 8) | ('b' << 16) | ('l' << 24),
+        DungeonHeavyGreenFog = ('F' << 0) | ('D' << 8) | ('g' << 16) | ('h' << 24),
+        DungeonGreenFog = ('F' << 0) | ('D' << 8) | ('g' << 16) | ('l' << 24),
+        DungeonHeavyRedFog = ('F' << 0) | ('D' << 8) | ('r' << 16) | ('h' << 24),
+        DungeonRedFog = ('F' << 0) | ('D' << 8) | ('r' << 16) | ('l' << 24),
+        DungeonHeavyWhiteFog = ('F' << 0) | ('D' << 8) | ('w' << 16) | ('h' << 24),
+        DungeonWhiteFog = ('F' << 0) | ('D' << 8) | ('w' << 16) | ('l' << 24),
+        LordaeronHeavyRain = ('R' << 0) | ('L' << 8) | ('h' << 16) | ('r' << 24),
+        LordaeronLightRain = ('R' << 0) | ('L' << 8) | ('l' << 16) | ('r' << 24),
+        NorthrendBlizzard = ('S' << 0) | ('N' << 8) | ('b' << 16) | ('s' << 24),
+        NorthrendHeavySnow = ('S' << 0) | ('N' << 8) | ('h' << 16) | ('s' << 24),
+        NorthrendLightSnow = ('S' << 0) | ('N' << 8) | ('l' << 16) | ('s' << 24),
+        RaysOfLight = ('L' << 0) | ('R' << 8) | ('a' << 16) | ('a' << 24),
+        RaysOfMoonlight = ('L' << 0) | ('R' << 8) | ('m' << 16) | ('a' << 24),
+        Wind = ('W' << 0) | ('N' << 8) | ('c' << 16) | ('w' << 24),
+        OutlandWind = ('W' << 0) | ('O' << 8) | ('c' << 16) | ('w' << 24),
+        OutlandWindLight = ('W' << 0) | ('O' << 8) | ('l' << 16) | ('w' << 24),
     }
 }
