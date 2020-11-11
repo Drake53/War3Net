97b16f97c4b8d9e4783ed501b42573bdf0090c0f
Drake53 <49623303+Drake53@users.noreply.github.com>
2020-11-11 16:04:32 +0100

    Implement war3map.wts parser/serializer.

---
+++ src/War3Net.Build.Core/Script/MapTriggerString.cs
@@ -0,0 +1,138 @@
+﻿// ------------------------------------------------------------------------------
+// <copyright file="MapTriggerString.cs" company="Drake53">
+// Licensed under the MIT license.
+// See the LICENSE file in the project root for more information.
+// </copyright>
+// ------------------------------------------------------------------------------
+
+using System;
+using System.IO;
+
+namespace War3Net.Build.Script
+{
+    public sealed class MapTriggerString
+    {
+        private uint _emptyLines;
+        private uint _keyLength;
+        private uint _key;
+        private string? _comment;
+        private string? _value;
+
+        private MapTriggerString()
+        {
+        }
+
+        public uint Key => _key;
+
+        public string? Comment => _comment;
+
+        public string? Value => _value;
+
+        public static MapTriggerString Parse(Stream stream, bool leaveOpen)
+        {
+            using (var reader = new StreamReader(stream, leaveOpen: leaveOpen))
+            {
+                return ReadFrom(reader);
+            }
+        }
+
+        public static MapTriggerString ReadFrom(StreamReader reader)
+        {
+            var triggerString = new MapTriggerString();
+
+            while (true)
+            {
+                var line = reader.ReadLine();
+                if (line is null)
+                {
+                    break;
+                }
+
+                if (line.StartsWith("STRING ", StringComparison.Ordinal))
+                {
+                    // Read key
+                    var keyString = line[7..].Trim();
+                    triggerString._keyLength = (uint)keyString.Length;
+                    triggerString._key = uint.TryParse(keyString, out var result) ? result : 0;
+
+                    // Read comment
+                    var isFirstLine = true;
+                    while (true)
+                    {
+                        line = reader.ReadLine();
+                        if (string.Equals(line, "{", StringComparison.Ordinal))
+                        {
+                            break;
+                        }
+                        else if (line.StartsWith("//", StringComparison.Ordinal) || triggerString._comment is not null)
+                        {
+                            if (isFirstLine)
+                            {
+                                triggerString._comment = line[2..];
+                                isFirstLine = false;
+                            }
+                            else
+                            {
+                                triggerString._comment += $"\r\n{line}";
+                            }
+                        }
+                        else
+                        {
+                            throw new InvalidDataException("Expected opening brace or comment.");
+                        }
+                    }
+
+                    // Read value
+                    isFirstLine = true;
+                    while (true)
+                    {
+                        line = reader.ReadLine();
+                        if (string.Equals(line, "}", StringComparison.Ordinal))
+                        {
+                            break;
+                        }
+
+                        if (isFirstLine)
+                        {
+                            triggerString._value = line;
+                            isFirstLine = false;
+                        }
+                        else
+                        {
+                            triggerString._value += $"\r\n{line}";
+                        }
+                    }
+
+                    break;
+                }
+                else
+                {
+                    triggerString._emptyLines++;
+                }
+            }
+
+            return triggerString;
+        }
+
+        public void WriteTo(StreamWriter writer)
+        {
+            for (var i = 0; i < _emptyLines; i++)
+            {
+                writer.WriteLine();
+            }
+
+            if (_value is not null)
+            {
+                writer.WriteLine($"STRING {_key.ToString($"D{_keyLength}")}");
+                if (_comment is not null)
+                {
+                    writer.WriteLine($"//{_comment}");
+                }
+
+                writer.WriteLine("{");
+                writer.WriteLine(_value);
+                writer.WriteLine("}");
+            }
+        }
+    }
+}
---
+++ src/War3Net.Build.Core/Script/MapTriggerStrings.cs
@@ -0,0 +1,126 @@
+﻿// ------------------------------------------------------------------------------
+// <copyright file="MapTriggerStrings.cs" company="Drake53">
+// Licensed under the MIT license.
+// See the LICENSE file in the project root for more information.
+// </copyright>
+// ------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace War3Net.Build.Script
+{
+    public sealed class MapTriggerStrings
+    {
+        public const string FileName = "war3map.wts";
+
+        private static readonly List<Encoding> _possibleEncodings = GetPossibleEncodings().ToList();
+
+        private readonly List<MapTriggerString> _strings;
+
+        private Encoding _encoding;
+
+        private MapTriggerStrings()
+        {
+            _strings = new List<MapTriggerString>();
+        }
+
+        public static bool IsRequired => false;
+
+        public MapTriggerString this[uint key] => _strings.First(s => s.Key == key);
+
+        public static MapTriggerStrings Parse(Stream stream, Encoding? encoding = null, bool leaveOpen = false)
+        {
+            try
+            {
+                var triggerStrings = new MapTriggerStrings();
+
+                var position = stream.Position;
+                var decoderExceptions = new List<DecoderFallbackException>();
+
+                var encodings = new List<Encoding>();
+                if (encoding is not null)
+                {
+                    encodings.Add(encoding);
+                }
+
+                encodings.AddRange(_possibleEncodings);
+                foreach (var encodingToTry in encodings)
+                {
+                    try
+                    {
+                        using (var reader = new StreamReader(stream, encodingToTry, leaveOpen: true))
+                        {
+                            while (!reader.EndOfStream)
+                            {
+                                triggerStrings._strings.Add(MapTriggerString.ReadFrom(reader));
+                            }
+
+                            triggerStrings._encoding = reader.CurrentEncoding;
+                        }
+
+                        return triggerStrings;
+                    }
+                    catch (DecoderFallbackException e)
+                    {
+                        decoderExceptions.Add(e);
+                        triggerStrings._strings.Clear();
+                        stream.Position = position;
+                    }
+                }
+
+                throw new AggregateException("Unknown encoding.", decoderExceptions);
+            }
+            catch (DecoderFallbackException e)
+            {
+                throw new InvalidDataException($"The '{FileName}' file contains invalid characters.", e);
+            }
+            catch (EndOfStreamException e)
+            {
+                throw new InvalidDataException($"The '{FileName}' file is missing data, or its data is invalid.", e);
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                if (!leaveOpen)
+                {
+                    stream.Dispose();
+                }
+            }
+        }
+
+        public static void Serialize(MapTriggerStrings mapTriggerStrings, Stream stream, bool leaveOpen = false)
+        {
+            mapTriggerStrings.SerializeTo(stream, leaveOpen);
+        }
+
+        public void SerializeTo(Stream stream, bool leaveOpen = false)
+        {
+            using (var writer = new StreamWriter(stream, _encoding, leaveOpen: leaveOpen))
+            {
+                foreach (var triggerString in _strings)
+                {
+                    triggerString.WriteTo(writer);
+                }
+            }
+        }
+
+        private static IEnumerable<Encoding> GetPossibleEncodings()
+        {
+            yield return new UTF8Encoding(false, true);
+
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            var eucKR = CodePagesEncodingProvider.Instance.GetEncoding(51949, EncoderFallback.ExceptionFallback, DecoderFallback.ExceptionFallback);
+            if (eucKR is not null)
+            {
+                yield return eucKR;
+            }
+        }
+    }
+}
