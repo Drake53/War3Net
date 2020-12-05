a4eef7d436a438887680220a0b2df02723b2c86c
Drake53 <49623303+Drake53@users.noreply.github.com>
2020-12-05 11:11:47 +0100

    Add map and campaign classes.

---
+++ src/War3Net.Build.Core/Campaign.cs
@@ -0,0 +1,34 @@
+﻿// ------------------------------------------------------------------------------
+// <copyright file="Campaign.cs" company="Drake53">
+// Licensed under the MIT license.
+// See the LICENSE file in the project root for more information.
+// </copyright>
+// ------------------------------------------------------------------------------
+
+using War3Net.Build.Info;
+using War3Net.Build.Object;
+using War3Net.Build.Script;
+
+namespace War3Net.Build
+{
+    public sealed class Campaign
+    {
+        public CampaignInfo Info { get; set; }
+
+        public CampaignAbilityObjectData? AbilityObjectData { get; set; }
+
+        public CampaignBuffObjectData? BuffObjectData { get; set; }
+
+        public CampaignDestructableObjectData? DestructableObjectData { get; set; }
+
+        public CampaignDoodadObjectData? DoodadObjectData { get; set; }
+
+        public CampaignItemObjectData? ItemObjectData { get; set; }
+
+        public CampaignUnitObjectData? UnitObjectData { get; set; }
+
+        public CampaignUpgradeObjectData? UpgradeObjectData { get; set; }
+
+        public CampaignTriggerStrings? TriggerStrings { get; set; }
+    }
+}
---
+++ src/War3Net.Build.Core/Map.cs
@@ -0,0 +1,64 @@
+﻿// ------------------------------------------------------------------------------
+// <copyright file="Map.cs" company="Drake53">
+// Licensed under the MIT license.
+// See the LICENSE file in the project root for more information.
+// </copyright>
+// ------------------------------------------------------------------------------
+
+using System.IO;
+using System.Text;
+
+using War3Net.Build.Audio;
+using War3Net.Build.Environment;
+using War3Net.Build.Extensions;
+using War3Net.Build.Info;
+using War3Net.Build.Object;
+using War3Net.Build.Script;
+using War3Net.Build.Widget;
+using War3Net.IO.Mpq;
+
+namespace War3Net.Build
+{
+    public sealed class Map
+    {
+        public MapSounds? Sounds { get; set; }
+
+        public MapCameras? Cameras { get; set; }
+
+        public MapEnvironment Environment { get; set; }
+
+        public MapPreviewIcons? PreviewIcons { get; set; }
+
+        public MapRegions? Regions { get; set; }
+
+        public PathingMap? PathingMap { get; set; }
+
+        public ShadowMap? ShadowMap { get; set; }
+
+        public MapInfo Info { get; set; }
+
+        public MapAbilityObjectData? AbilityObjectData { get; set; }
+
+        public MapBuffObjectData? BuffObjectData { get; set; }
+
+        public MapDestructableObjectData? DestructableObjectData { get; set; }
+
+        public MapDoodadObjectData? DoodadObjectData { get; set; }
+
+        public MapItemObjectData? ItemObjectData { get; set; }
+
+        public MapUnitObjectData? UnitObjectData { get; set; }
+
+        public MapUpgradeObjectData? UpgradeObjectData { get; set; }
+
+        public MapCustomTextTriggers? CustomTextTriggers { get; set; }
+
+        public MapTriggers? Triggers { get; set; }
+
+        public TriggerStrings? TriggerStrings { get; set; }
+
+        public MapDoodads? Doodads { get; set; }
+
+        public MapUnits? Units { get; set; }
+    }
+}
