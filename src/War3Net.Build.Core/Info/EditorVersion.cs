64f012067c54ea27b771f10de2d464778ee38049
Drake53 <49623303+Drake53@users.noreply.github.com>
2020-12-13 11:36:40 +0100

    Add EditorVersion enum.

---
+++ src/War3Net.Build.Core/Info/EditorVersion.cs
@@ -0,0 +1,31 @@
+﻿// ------------------------------------------------------------------------------
+// <copyright file="EditorVersion.cs" company="Drake53">
+// Licensed under the MIT license.
+// See the LICENSE file in the project root for more information.
+// </copyright>
+// ------------------------------------------------------------------------------
+
+namespace War3Net.Build.Info
+{
+    public enum EditorVersion
+    {
+        v6008 = 6008,
+        v6009 = 6009,
+        v6010 = 6010,
+        v6011 = 6011,
+        v6013 = 6013,
+        v6015 = 6015,
+        v6031 = 6031,
+        v6052 = 6052,
+        v6057 = 6057,
+        v6058 = 6058,
+        v6059 = 6059,
+        v6061 = 6061,
+        v6071 = 6071,
+        v6072 = 6072,
+        v6098 = 6098,
+        v6105 = 6105,
+        v6110 = 6110,
+        v6111 = 6111,
+    }
+}
