bad6d468469236c1b46cec1295e50f967ac195fb
Drake53 <49623303+Drake53@users.noreply.github.com>
2021-02-14 11:43:44 +0100

    Re-implement LegacyMapBuilder using the new MapBuilder.

---
+++ src/War3Net.Build/LegacyMapBuilder.cs
@@ -0,0 +1,165 @@
+﻿// ------------------------------------------------------------------------------
+// <copyright file="LegacyMapBuilder.cs" company="Drake53">
+// Licensed under the MIT license.
+// See the LICENSE file in the project root for more information.
+// </copyright>
+// ------------------------------------------------------------------------------
+
+using System;
+using System.IO;
+using System.Linq;
+
+using CSharpLua;
+
+using Microsoft.CodeAnalysis;
+
+using War3Net.Build.Extensions;
+using War3Net.Build.Info;
+using War3Net.IO.Mpq;
+
+namespace War3Net.Build
+{
+    [Obsolete]
+    public sealed class LegacyMapBuilder
+    {
+        private const string DefaultMapName = "TestMap.w3x";
+        private const ushort DefaultBlockSize = 3;
+        private const bool DefaultGenerateListFile = true;
+
+        private string _outputMapName;
+        private ushort _blockSize;
+        private bool _generateListFile;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LegacyMapBuilder"/> class.
+        /// </summary>
+        public LegacyMapBuilder()
+            : this(DefaultMapName, DefaultBlockSize, DefaultGenerateListFile)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LegacyMapBuilder"/> class.
+        /// </summary>
+        public LegacyMapBuilder(string mapName)
+            : this(mapName, DefaultBlockSize, DefaultGenerateListFile)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LegacyMapBuilder"/> class.
+        /// </summary>
+        public LegacyMapBuilder(string mapName, ushort blockSize)
+            : this(mapName, blockSize, DefaultGenerateListFile)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LegacyMapBuilder"/> class.
+        /// </summary>
+        public LegacyMapBuilder(string mapName, ushort blockSize, bool generateListFile)
+        {
+            _outputMapName = mapName;
+            _blockSize = blockSize;
+            _generateListFile = generateListFile;
+        }
+
+        [Obsolete("This event is no longer supported.", true)]
+        public event EventHandler<ArchiveBuildingEventArgs>? OnArchiveBuilding;
+
+        public string OutputMapName
+        {
+            get => _outputMapName;
+            set => _outputMapName = value;
+        }
+
+        public ushort BlockSize
+        {
+            get => _blockSize;
+            set => _blockSize = value;
+        }
+
+        public bool GenerateListFile
+        {
+            get => _generateListFile;
+            set => _generateListFile = value;
+        }
+
+        public BuildResult Build(ScriptCompilerOptions compilerOptions, params string[] assetsDirectories)
+        {
+            if (compilerOptions is null)
+            {
+                throw new ArgumentNullException(nameof(compilerOptions));
+            }
+
+            var map = new Map(compilerOptions.MapInfo!, compilerOptions.MapEnvironment!)
+            {
+                Sounds = compilerOptions.MapSounds,
+                PreviewIcons = compilerOptions.MapIcons,
+                Regions = compilerOptions.MapRegions,
+                AbilityObjectData = compilerOptions.MapAbilityData,
+                BuffObjectData = compilerOptions.MapBuffData,
+                DestructableObjectData = compilerOptions.MapDestructableData,
+                DoodadObjectData = compilerOptions.MapDoodadData,
+                ItemObjectData = compilerOptions.MapItemData,
+                UnitObjectData = compilerOptions.MapUnitData,
+                UpgradeObjectData = compilerOptions.MapUpgradeData,
+                Doodads = compilerOptions.MapDoodads,
+                Units = compilerOptions.MapUnits,
+                CustomTextTriggers = compilerOptions.MapCustomTextTriggers,
+                Triggers = compilerOptions.MapTriggers,
+                TriggerStrings = compilerOptions.MapTriggerStrings,
+                Cameras = compilerOptions.MapCameras,
+                PathingMap = compilerOptions.MapPathingMap,
+                ShadowMap = compilerOptions.MapShadowMap,
+            };
+
+            var builder = new MapBuilder(map);
+            foreach (var assetDirectory in assetsDirectories)
+            {
+                builder.AddFiles(assetDirectory);
+            }
+
+            if (map.Info.ScriptLanguage == ScriptLanguage.Lua)
+            {
+                var csc = compilerOptions.Debug ? "-define:DEBUG" : null;
+                var csproj = Directory.EnumerateFiles(compilerOptions.SourceDirectory, "*.csproj", SearchOption.TopDirectoryOnly).Single();
+                var compiler = new Compiler(csproj, compilerOptions.OutputDirectory, string.Empty, null, csc, false, null, string.Empty)
+                {
+                    IsExportMetadata = false,
+                    IsModule = false,
+                    IsInlineSimpleProperty = false,
+                    IsPreventDebugObject = true,
+                    IsCommentsDisabled = compilerOptions.Optimize,
+                    IsDecompilePackageLibs = compilerOptions.DecompilePackageLibs,
+                };
+
+                var compileResult = string.IsNullOrEmpty(compilerOptions.CommonJPath) || string.IsNullOrEmpty(compilerOptions.BlizzardJPath)
+                    ? map.CompileScript(compiler, compilerOptions.Libraries)
+                    : map.CompileScript(compiler, compilerOptions.Libraries, compilerOptions.CommonJPath, compilerOptions.BlizzardJPath);
+
+                if (!compileResult.Success)
+                {
+                    return new BuildResult(false, compileResult, Array.Empty<Diagnostic>());
+                }
+            }
+            else
+            {
+                map.CompileScript();
+            }
+
+            // OnArchiveBuilding?.Invoke(this, new ArchiveBuildingEventArgs(mpqFiles));
+
+            var archiveCreateOptions = new MpqArchiveCreateOptions
+            {
+                ListFileCreateMode = _generateListFile ? MpqFileCreateMode.Overwrite : MpqFileCreateMode.Prune,
+                AttributesCreateMode = MpqFileCreateMode.Prune,
+                BlockSize = _blockSize,
+            };
+
+            builder.Build(Path.Combine(compilerOptions.OutputDirectory, _outputMapName), archiveCreateOptions);
+
+            return new BuildResult(true, null, Array.Empty<Diagnostic>());
+        }
+    }
+}
---
+++ src/War3Net.Build/ScriptCompilerOptions.cs
@@ -0,0 +1,215 @@
+﻿// ------------------------------------------------------------------------------
+// <copyright file="ScriptCompilerOptions.cs" company="Drake53">
+// Licensed under the MIT license.
+// See the LICENSE file in the project root for more information.
+// </copyright>
+// ------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using War3Net.Build.Audio;
+using War3Net.Build.Common;
+using War3Net.Build.Environment;
+using War3Net.Build.Info;
+using War3Net.Build.Object;
+using War3Net.Build.Script;
+using War3Net.Build.Widget;
+using War3Net.IO.Mpq;
+
+namespace War3Net.Build
+{
+    [Obsolete]
+    public sealed class ScriptCompilerOptions
+    {
+        public ScriptCompilerOptions(params string[] libraries)
+        {
+            FileFlags = new Dictionary<string, MpqFileFlags>();
+            DefaultFileFlags = MpqFileFlags.Exists;
+            Libraries = new List<string>(libraries);
+
+            SourceDirectory = string.Empty;
+            OutputDirectory = string.Empty;
+        }
+
+        public ScriptCompilerOptions(IEnumerable<string> libraries)
+            : this(libraries.ToArray())
+        {
+        }
+
+        public string SourceDirectory { get; set; }
+
+        public string OutputDirectory { get; set; }
+
+        /// <summary>
+        /// Gets or sets the path to clijasshelper.exe.
+        /// This property must be set when compiling vJass source code.
+        /// </summary>
+        public string? JasshelperCliPath { get; set; }
+
+        public string? CommonJPath { get; set; }
+
+        public string? BlizzardJPath { get; set; }
+
+        /// <summary>
+        /// If <see langword="true"/>, script files added through assets will be overwritten, even if <see cref="SourceDirectory"/> is null.
+        /// </summary>
+        public bool ForceCompile { get; set; }
+
+        /// <summary>
+        /// If <see langword="true"/>, .dll files from package references are decompiled into .cs files, so that they can be transpiled to lua.
+        /// </summary>
+        public bool DecompilePackageLibs { get; set; }
+
+        public bool Debug { get; set; }
+
+        public bool Optimize { get; set; }
+
+        public bool Obfuscate { get; set; }
+
+        public Dictionary<string, MpqFileFlags> FileFlags { get; private set; }
+
+        public MpqFileFlags DefaultFileFlags { get; set; }
+
+        public MapInfo? MapInfo { get; set; }
+
+        public MapEnvironment? MapEnvironment { get; set; }
+
+        public MapDoodads? MapDoodads { get; set; }
+
+        public MapUnits? MapUnits { get; set; }
+
+        public MapRegions? MapRegions { get; set; }
+
+        public MapSounds? MapSounds { get; set; }
+
+        public MapPreviewIcons? MapIcons { get; set; }
+
+        public UnitObjectData? MapUnitData { get; set; }
+
+        public ItemObjectData? MapItemData { get; set; }
+
+        public DestructableObjectData? MapDestructableData { get; set; }
+
+        public DoodadObjectData? MapDoodadData { get; set; }
+
+        public AbilityObjectData? MapAbilityData { get; set; }
+
+        public BuffObjectData? MapBuffData { get; set; }
+
+        public UpgradeObjectData? MapUpgradeData { get; set; }
+
+        public MapCustomTextTriggers? MapCustomTextTriggers { get; set; }
+
+        public MapTriggers? MapTriggers { get; set; }
+
+        public TriggerStrings? MapTriggerStrings { get; set; }
+
+        public MapCameras? MapCameras { get; set; }
+
+        public MapPathingMap? MapPathingMap { get; set; }
+
+        public MapShadowMap? MapShadowMap { get; set; }
+
+        public string? LobbyMusic { get; set; }
+
+        public GamePatch? TargetPatch { get; set; }
+
+        internal List<string> Libraries { get; private set; }
+
+        public object? GetMapFile(string fileName)
+        {
+            return fileName switch
+            {
+                MapInfo.FileName => MapInfo,
+                MapEnvironment.FileName => MapEnvironment,
+                MapDoodads.FileName => MapDoodads,
+                MapUnits.FileName => MapUnits,
+                MapRegions.FileName => MapRegions,
+                MapSounds.FileName => MapSounds,
+                MapPreviewIcons.FileName => MapIcons,
+
+                MapUnitObjectData.FileName => MapUnitData,
+                MapItemObjectData.FileName => MapItemData,
+                MapDestructableObjectData.FileName => MapDestructableData,
+                MapDoodadObjectData.FileName => MapDoodadData,
+                MapAbilityObjectData.FileName => MapAbilityData,
+                MapBuffObjectData.FileName => MapBuffData,
+                MapUpgradeObjectData.FileName => MapUpgradeData,
+
+                _ => null,
+            };
+        }
+
+        public bool SetMapFile(object file)
+        {
+            if (file is null)
+            {
+                return false;
+            }
+            else if (file is MapInfo mapInfo)
+            {
+                MapInfo = mapInfo;
+            }
+            else if (file is MapEnvironment mapEnvironment)
+            {
+                MapEnvironment = mapEnvironment;
+            }
+            else if (file is MapDoodads mapDoodads)
+            {
+                MapDoodads = mapDoodads;
+            }
+            else if (file is MapUnits mapUnits)
+            {
+                MapUnits = mapUnits;
+            }
+            else if (file is MapRegions mapRegions)
+            {
+                MapRegions = mapRegions;
+            }
+            else if (file is MapSounds mapSounds)
+            {
+                MapSounds = mapSounds;
+            }
+            else if (file is MapPreviewIcons mapIcons)
+            {
+                MapIcons = mapIcons;
+            }
+            else if (file is MapUnitObjectData mapUnitData)
+            {
+                MapUnitData = mapUnitData;
+            }
+            else if (file is MapItemObjectData mapItemData)
+            {
+                MapItemData = mapItemData;
+            }
+            else if (file is MapDestructableObjectData mapDestructableData)
+            {
+                MapDestructableData = mapDestructableData;
+            }
+            else if (file is MapDoodadObjectData mapDoodadData)
+            {
+                MapDoodadData = mapDoodadData;
+            }
+            else if (file is MapAbilityObjectData mapAbilityData)
+            {
+                MapAbilityData = mapAbilityData;
+            }
+            else if (file is MapBuffObjectData mapBuffData)
+            {
+                MapBuffData = mapBuffData;
+            }
+            else if (file is MapUpgradeObjectData mapUpgradeData)
+            {
+                MapUpgradeData = mapUpgradeData;
+            }
+            else
+            {
+                throw new ArgumentException("The object is not a map file.", nameof(file));
+            }
+
+            return true;
+        }
+    }
+}
