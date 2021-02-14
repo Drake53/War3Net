70ff7a0fc23614b783564a7cb4587543d9e31dbd
Drake53 <49623303+Drake53@users.noreply.github.com>
2021-02-14 11:42:16 +0100

    Implement new MapBuilder.

--- src/War3Net.Build/Extensions/MapExtensions.cs
+++ src/War3Net.Build/Extensions/MapExtensions.cs
@@ -5,8 +5,12 @@
 // </copyright>
 // ------------------------------------------------------------------------------
 
+using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Text;
+
+using CSharpLua;
 
 using War3Net.Build.Audio;
 using War3Net.Build.Environment;
@@ -14,6 +18,8 @@ using War3Net.Build.Info;
 using War3Net.Build.Object;
 using War3Net.Build.Script;
 using War3Net.Build.Widget;
+using War3Net.CodeAnalysis.Jass;
+using War3Net.CodeAnalysis.Transpilers;
 using War3Net.IO.Mpq;
 
 namespace War3Net.Build.Extensions
@@ -509,6 +515,99 @@ namespace War3Net.Build.Extensions
             }
 
             return true;
+        }
+
+        public static void CompileScript(this Map map)
+        {
+            if (map is null)
+            {
+                throw new ArgumentNullException(nameof(map));
+            }
+
+            if (map.Info.ScriptLanguage != ScriptLanguage.Jass)
+            {
+                throw new InvalidOperationException($"The map's script language must be set to jass in order to use the jass compiler.");
+            }
+
+            using var stream = new MemoryStream();
+
+            var mapScriptBuilder = new MapScriptBuilder();
+            mapScriptBuilder.SetDefaultOptionsForMap(map);
+
+            var compilationUnit = mapScriptBuilder.Build(map);
+
+            using (var writer = new StreamWriter(stream, _defaultEncoding, leaveOpen: true))
+            {
+                var renderer = new JassRenderer(writer);
+                renderer.Render(compilationUnit);
+            }
+
+            stream.Position = 0;
+            map.SetScriptFile(stream);
+        }
+
+        public static CompileResult CompileScript(this Map map, Compiler compiler, IEnumerable<string> luaSystemLibs)
+        {
+            var jassHelperFolder = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments), "Warcraft III", "JassHelper");
+            return map.CompileScript(compiler, luaSystemLibs, Path.Combine(jassHelperFolder, "common.j"), Path.Combine(jassHelperFolder, "Blizzard.j"));
+        }
+
+        public static CompileResult CompileScript(this Map map, Compiler compiler, IEnumerable<string> luaSystemLibs, string commonJPath, string blizzardJPath)
+        {
+            if (map is null)
+            {
+                throw new ArgumentNullException(nameof(map));
+            }
+
+            if (compiler is null)
+            {
+                throw new ArgumentNullException(nameof(compiler));
+            }
+
+            if (map.Info.ScriptLanguage != ScriptLanguage.Lua)
+            {
+                throw new InvalidOperationException($"The map's script language must be set to lua in order to use the C# compiler.");
+            }
+
+            using var stream = new MemoryStream();
+
+            try
+            {
+                compiler.CompileSingleFile(stream, luaSystemLibs);
+            }
+            catch (CompilationErrorException e)
+            {
+                return new CompileResult(e.EmitResult);
+            }
+
+            var transpiler = new JassToLuaTranspiler();
+            transpiler.IgnoreComments = true;
+            transpiler.IgnoreEmptyDeclarations = true;
+            transpiler.IgnoreEmptyStatements = true;
+            transpiler.KeepFunctionsSeparated = true;
+
+            transpiler.RegisterJassFile(JassSyntaxFactory.ParseCompilationUnit(File.ReadAllText(commonJPath)));
+            transpiler.RegisterJassFile(JassSyntaxFactory.ParseCompilationUnit(File.ReadAllText(blizzardJPath)));
+
+            var mapScriptBuilder = new MapScriptBuilder();
+            mapScriptBuilder.SetDefaultOptionsForCSharpLua();
+
+            var luaCompilationUnit = transpiler.Transpile(mapScriptBuilder.Build(map));
+            using (var writer = new StreamWriter(stream, _defaultEncoding, leaveOpen: true))
+            {
+                var luaRenderOptions = new LuaSyntaxGenerator.SettingInfo
+                {
+                    Indent = 4,
+                };
+
+                var luaRenderer = new LuaRenderer(luaRenderOptions, writer);
+                luaRenderer.RenderCompilationUnit(luaCompilationUnit);
+            }
+
+            stream.Position = 0;
+            map.SetScriptFile(stream);
+
+            return new CompileResult(true, null);
         }
     }
 }
---
+++ src/War3Net.Build/MapBuilder.cs
@@ -0,0 +1,160 @@
+﻿// ------------------------------------------------------------------------------
+// <copyright file="MapBuilder.cs" company="Drake53">
+// Licensed under the MIT license.
+// See the LICENSE file in the project root for more information.
+// </copyright>
+// ------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+using CSharpLua;
+
+using War3Net.Build.Extensions;
+using War3Net.IO.Mpq;
+
+namespace War3Net.Build
+{
+    public class MapBuilder
+    {
+        private readonly Map _map;
+        private readonly Encoding? _encoding;
+        private readonly List<MpqFile> _files;
+
+        public MapBuilder(Map map)
+        {
+            _map = map;
+            _encoding = null;
+            _files = new List<MpqFile>();
+        }
+
+        public void AddFiles(MpqArchive archive)
+        {
+            archive.DiscoverFileNames();
+            AddFiles(archive.GetMpqFiles());
+        }
+
+        public void AddFiles(params MpqFile[] files)
+        {
+            AddFiles((IEnumerable<MpqFile>)files);
+        }
+
+        public void AddFiles(IEnumerable<MpqFile> files)
+        {
+            foreach (var file in files)
+            {
+                if (file is not MpqKnownFile knownFile || !_map.SetFile(knownFile.FileName, false, knownFile.MpqStream))
+                {
+                    _files.Add(file);
+                }
+            }
+        }
+
+        public void AddFiles(string directory)
+        {
+            AddFiles(directory, Directory.EnumerateFiles(directory));
+        }
+
+        public void AddFiles(string directory, string searchPattern)
+        {
+            AddFiles(directory, Directory.EnumerateFiles(directory, searchPattern));
+        }
+
+        public void AddFiles(string directory, string searchPattern, EnumerationOptions enumerationOptions)
+        {
+            AddFiles(directory, Directory.EnumerateFiles(directory, searchPattern, enumerationOptions));
+        }
+
+        public void AddFiles(string directory, string searchPattern, SearchOption searchOption)
+        {
+            AddFiles(directory, Directory.EnumerateFiles(directory, searchPattern, searchOption));
+        }
+
+        public void Compile(Compiler compiler, IEnumerable<string> luaSystemLibs)
+        {
+            _map.CompileScript(compiler, luaSystemLibs);
+        }
+
+        public void Build(string path)
+        {
+            CreateArchiveBuilder().SaveWithPreArchiveData(path);
+        }
+
+        public void Build(string path, MpqArchiveCreateOptions createOptions)
+        {
+            CreateArchiveBuilder().SaveWithPreArchiveData(path, createOptions);
+        }
+
+        public void Build(Stream stream, bool leaveOpen = false)
+        {
+            CreateArchiveBuilder().SaveWithPreArchiveData(stream, leaveOpen);
+        }
+
+        public void Build(Stream stream, MpqArchiveCreateOptions createOptions, bool leaveOpen = false)
+        {
+            CreateArchiveBuilder().SaveWithPreArchiveData(stream, createOptions, leaveOpen);
+        }
+
+        protected virtual MpqArchiveBuilder CreateArchiveBuilder()
+        {
+            var archiveBuilder = new MpqArchiveBuilder();
+            void AddFilesToArchiveBuilder(params MpqFile?[] files)
+            {
+                foreach (var file in files)
+                {
+                    if (file is not null)
+                    {
+                        archiveBuilder!.AddFile(file);
+                    }
+                }
+            }
+
+            AddFilesToArchiveBuilder(
+                _map.GetDoodadsFile(_encoding),
+                _map.GetScriptFile(_encoding),
+                _map.GetPreviewIconsFile(_encoding),
+                _map.GetShadowMapFile(_encoding),
+                _map.GetAbilityObjectDataFile(_encoding),
+                _map.GetDestructableObjectDataFile(_encoding),
+                _map.GetCamerasFile(_encoding),
+                _map.GetDoodadObjectDataFile(_encoding),
+                _map.GetEnvironmentFile(_encoding),
+                _map.GetBuffObjectDataFile(_encoding),
+                _map.GetInfoFile(_encoding),
+                _map.GetUpgradeObjectDataFile(_encoding),
+                _map.GetRegionsFile(_encoding),
+                _map.GetSoundsFile(_encoding),
+                _map.GetItemObjectDataFile(_encoding),
+                _map.GetUnitObjectDataFile(_encoding),
+                _map.GetCustomTextTriggersFile(_encoding),
+                _map.GetPathingMapFile(_encoding),
+                _map.GetTriggersFile(_encoding),
+                _map.GetTriggerStringsFile(_encoding),
+                _map.GetUnitsFile(_encoding));
+
+            AddFilesToArchiveBuilder(_files.ToArray());
+
+            return archiveBuilder;
+        }
+
+        private void AddFiles(string baseDirectory, IEnumerable<string> files)
+        {
+            var baseDirectoryLength = baseDirectory.Length;
+            if (!baseDirectory.EndsWith('/') && !baseDirectory.EndsWith('\\'))
+            {
+                baseDirectoryLength++;
+            }
+
+            foreach (var file in files)
+            {
+                var fileName = file[baseDirectoryLength..];
+                var fileStream = File.OpenRead(file);
+                if (!_map.SetFile(fileName, false, fileStream))
+                {
+                    _files.Add(MpqFile.New(fileStream, fileName));
+                }
+            }
+        }
+    }
+}
