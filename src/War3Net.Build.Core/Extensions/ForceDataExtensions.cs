7f967683af98a0749a7c7a3218bf93075551170a
Drake53 <49623303+Drake53@users.noreply.github.com>
2021-02-16 17:40:40 +0100

    Add SetPlayers extension method.

---
+++ src/War3Net.Build.Core/Extensions/ForceDataExtensions.cs
@@ -0,0 +1,22 @@
+﻿// ------------------------------------------------------------------------------
+// <copyright file="ForceDataExtensions.cs" company="Drake53">
+// Licensed under the MIT license.
+// See the LICENSE file in the project root for more information.
+// </copyright>
+// ------------------------------------------------------------------------------
+
+using System.Linq;
+
+using War3Net.Build.Common;
+using War3Net.Build.Info;
+
+namespace War3Net.Build.Extensions
+{
+    public static class ForceDataExtensions
+    {
+        public static void SetPlayers(this ForceData force, params PlayerData[] players)
+        {
+            force.Players = new Bitmask32(players.Select(player => player.Id).ToArray());
+        }
+    }
+}
