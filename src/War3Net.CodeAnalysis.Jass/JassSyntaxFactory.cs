f5532e82ad477152d1d19a10a34ff4cd6daceca8
Drake53 <49623303+Drake53@users.noreply.github.com>
2021-12-18 12:43:18 +0100

    Add TryParse methods.

--- src/War3Net.CodeAnalysis.Jass/JassSyntaxFactory.cs
+++ src/War3Net.CodeAnalysis.Jass/JassSyntaxFactory.cs
@@ -5,6 +5,8 @@
 // </copyright>
 // ------------------------------------------------------------------------------
 
+using System.Diagnostics.CodeAnalysis;
+
 using Pidgin;
 
 using War3Net.CodeAnalysis.Jass.Syntax;
@@ -61,6 +63,70 @@ namespace War3Net.CodeAnalysis.Jass
         public static JassTypeSyntax ParseTypeName(string typeName)
         {
             return JassParser.Instance.TypeParser.ParseOrThrow(typeName);
+        }
+
+        public static bool TryParseArgumentList(string argumentList, [NotNullWhen(true)] out JassArgumentListSyntax? result)
+        {
+            return TryParse(argumentList, JassParser.Instance.ArgumentListParser, out result);
+        }
+
+        public static bool TryParseCompilationUnit(string compilationUnit, [NotNullWhen(true)] out JassCompilationUnitSyntax? result)
+        {
+            return TryParse(compilationUnit, JassParser.Instance.CompilationUnitParser, out result);
+        }
+
+        public static bool TryParseCustomScriptAction(string customScriptAction, [NotNullWhen(true)] out ICustomScriptAction? result)
+        {
+            return TryParse(customScriptAction, JassParser.Instance.CustomScriptActionParser, out result);
+        }
+
+        public static bool TryParseDeclaration(string declaration, [NotNullWhen(true)] out IDeclarationSyntax? result)
+        {
+            return TryParse(declaration, JassParser.Instance.DeclarationParser, out result);
+        }
+
+        public static bool TryParseExpression(string expression, [NotNullWhen(true)] out IExpressionSyntax? result)
+        {
+            return TryParse(expression, JassParser.Instance.ExpressionParser, out result);
+        }
+
+        public static bool TryParseFunctionDeclaration(string functionDeclaration, [NotNullWhen(true)] out JassFunctionDeclarationSyntax? result)
+        {
+            return TryParse(functionDeclaration, JassParser.Instance.FunctionDeclarationParser, out result);
+        }
+
+        public static bool TryParseIdentifierName(string identifierName, [NotNullWhen(true)] out JassIdentifierNameSyntax? result)
+        {
+            return TryParse(identifierName, JassParser.Instance.IdentifierNameParser, out result);
+        }
+
+        public static bool TryParseParameterList(string parameterList, [NotNullWhen(true)] out JassParameterListSyntax? result)
+        {
+            return TryParse(parameterList, JassParser.Instance.ParameterListParser, out result);
+        }
+
+        public static bool TryParseStatement(string statement, [NotNullWhen(true)] out IStatementSyntax? result)
+        {
+            return TryParse(statement, JassParser.Instance.StatementParser, out result);
+        }
+
+        public static bool TryParseTypeName(string typeName, [NotNullWhen(true)] out JassTypeSyntax? result)
+        {
+            return TryParse(typeName, JassParser.Instance.TypeParser, out result);
+        }
+
+        private static bool TryParse<TSyntax>(string input, Parser<char, TSyntax> parser, [NotNullWhen(true)] out TSyntax? result)
+            where TSyntax : class
+        {
+            var parseResult = parser.Parse(input);
+            if (parseResult.Success)
+            {
+                result = parseResult.Value;
+                return true;
+            }
+
+            result = null;
+            return false;
         }
     }
 }
