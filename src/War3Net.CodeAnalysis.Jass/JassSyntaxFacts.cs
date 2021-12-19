e2665507b5c1b0c30cd65e0af15ca61e0148a89b
Drake53 <49623303+Drake53@users.noreply.github.com>
2021-12-19 18:05:56 +0100

    Add JassSyntaxFacts class.

---
+++ src/War3Net.CodeAnalysis.Jass/JassSyntaxFacts.cs
@@ -0,0 +1,74 @@
+﻿// ------------------------------------------------------------------------------
+// <copyright file="JassSyntaxFacts.cs" company="Drake53">
+// Licensed under the MIT license.
+// See the LICENSE file in the project root for more information.
+// </copyright>
+// ------------------------------------------------------------------------------
+
+using System.Diagnostics.CodeAnalysis;
+
+namespace War3Net.CodeAnalysis.Jass
+{
+    public static class JassSyntaxFacts
+    {
+        /// <summary>
+        /// Returns <see langword="true"/> if the character can be the starting character of a JASS identifier.
+        /// </summary>
+        /// <param name="ch">The character.</param>
+        public static bool IsIdentifierStartCharacter(char ch)
+        {
+            return (ch >= 'A' && ch <= 'Z')
+                || (ch >= 'a' && ch <= 'z');
+        }
+
+        /// <summary>
+        /// Returns <see langword="true"/> if the character can be a part of a JASS identifier.
+        /// </summary>
+        /// <param name="ch">The character.</param>
+        public static bool IsIdentifierPartCharacter(char ch)
+        {
+            return (ch >= 'A' && ch <= 'Z')
+                || (ch >= 'a' && ch <= 'z')
+                || (ch >= '0' && ch <= '9')
+                || ch == '_';
+        }
+
+        /// <summary>
+        /// Returns <see langword="true"/> if the character can be the ending character of a JASS identifier.
+        /// </summary>
+        /// <param name="ch">The character.</param>
+        public static bool IsIdentifierEndCharacter(char ch)
+        {
+            return (ch >= 'A' && ch <= 'Z')
+                || (ch >= 'a' && ch <= 'z')
+                || (ch >= '0' && ch <= '9');
+        }
+
+        /// <summary>
+        /// Check that the name is a valid identifier.
+        /// </summary>
+        public static bool IsValidIdentifier([NotNullWhen(true)] string? name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                return false;
+            }
+
+            if (!IsIdentifierStartCharacter(name[0]))
+            {
+                return false;
+            }
+
+            var lastPartCharacterIndex = name.Length - 1;
+            for (var i = 1; i < lastPartCharacterIndex; i++)
+            {
+                if (!IsIdentifierPartCharacter(name[i]))
+                {
+                    return false;
+                }
+            }
+
+            return IsIdentifierEndCharacter(name[^1]);
+        }
+    }
+}
---
+++ tests/War3Net.CodeAnalysis.Jass.Tests/JassSyntaxFactsTests.cs
@@ -0,0 +1,50 @@
+﻿// ------------------------------------------------------------------------------
+// <copyright file="JassSyntaxFactsTests.cs" company="Drake53">
+// Licensed under the MIT license.
+// See the LICENSE file in the project root for more information.
+// </copyright>
+// ------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace War3Net.CodeAnalysis.Jass.Tests
+{
+    [TestClass]
+    public class JassSyntaxFactsTests
+    {
+        [DataTestMethod]
+        [DynamicData(nameof(GetValidIdentifiers), DynamicDataSourceType.Method)]
+        public void TestValidIdentifiers(string? identifier)
+        {
+            Assert.IsTrue(JassSyntaxFacts.IsValidIdentifier(identifier));
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(GetInvalidIdentifiers), DynamicDataSourceType.Method)]
+        public void TestInvalidIdentifiers(string? identifier)
+        {
+            Assert.IsFalse(JassSyntaxFacts.IsValidIdentifier(identifier));
+        }
+
+        private static IEnumerable<object?[]> GetValidIdentifiers()
+        {
+            yield return new object?[] { "foo" };
+            yield return new object?[] { "f00" };
+            yield return new object?[] { "foo_bar" };
+            yield return new object?[] { "FOO" };
+            yield return new object?[] { "F_0" };
+        }
+
+        private static IEnumerable<object?[]> GetInvalidIdentifiers()
+        {
+            yield return new object?[] { null };
+            yield return new object?[] { string.Empty };
+            yield return new object?[] { "_foo" };
+            yield return new object?[] { "foo_" };
+            yield return new object?[] { "9foo" };
+            yield return new object?[] { "foo bar" };
+        }
+    }
+}
