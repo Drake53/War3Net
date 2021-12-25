9d894895e8a2fd02fb26c00f4c1dd3f5d368ae0e
Drake53 <49623303+Drake53@users.noreply.github.com>
2021-12-25 13:46:26 +0100

    Add extension method to localize strings in MapInfo.

---
+++ src/War3Net.Build.Core/Extensions/MapExtensions.cs
@@ -0,0 +1,58 @@
+﻿// ------------------------------------------------------------------------------
+// <copyright file="MapExtensions.cs" company="Drake53">
+// Licensed under the MIT license.
+// See the LICENSE file in the project root for more information.
+// </copyright>
+// ------------------------------------------------------------------------------
+
+namespace War3Net.Build.Extensions
+{
+    public static class MapExtensions
+    {
+        public static void LocalizeInfo(this Map map)
+        {
+            var info = map.Info;
+            var strings = map.TriggerStrings;
+            if (info is null || strings is null)
+            {
+                return;
+            }
+
+            if (strings.TryGetValue(info.MapName, out var mapName))
+            {
+                info.MapName = mapName;
+            }
+
+            if (strings.TryGetValue(info.MapAuthor, out var mapAuthor))
+            {
+                info.MapAuthor = mapAuthor;
+            }
+
+            if (strings.TryGetValue(info.MapDescription, out var mapDescription))
+            {
+                info.MapDescription = mapDescription;
+            }
+
+            if (strings.TryGetValue(info.RecommendedPlayers, out var recommendedPlayers))
+            {
+                info.RecommendedPlayers = recommendedPlayers;
+            }
+
+            foreach (var player in info.Players)
+            {
+                if (strings.TryGetValue(player.Name, out var name))
+                {
+                    player.Name = name;
+                }
+            }
+
+            foreach (var force in info.Forces)
+            {
+                if (strings.TryGetValue(force.Name, out var name))
+                {
+                    force.Name = name;
+                }
+            }
+        }
+    }
+}
---
+++ src/War3Net.Build.Core/Extensions/TriggerStringsExtensions.cs
@@ -0,0 +1,35 @@
+﻿// ------------------------------------------------------------------------------
+// <copyright file="TriggerStringsExtensions.cs" company="Drake53">
+// Licensed under the MIT license.
+// See the LICENSE file in the project root for more information.
+// </copyright>
+// ------------------------------------------------------------------------------
+
+using System;
+using System.Linq;
+
+using War3Net.Build.Script;
+
+namespace War3Net.Build.Extensions
+{
+    public static class TriggerStringsExtensions
+    {
+        public static bool TryGetValue(this TriggerStrings triggerStrings, string? trigstr, out string? value)
+        {
+            if (trigstr is not null &&
+                trigstr.StartsWith("TRIGSTR_", StringComparison.Ordinal) &&
+                uint.TryParse(trigstr["TRIGSTR_".Length..], out var key))
+            {
+                var triggerString = triggerStrings.Strings.FirstOrDefault(s => s.Key == key);
+                if (triggerString is not null)
+                {
+                    value = triggerString.Value;
+                    return true;
+                }
+            }
+
+            value = null;
+            return false;
+        }
+    }
+}
