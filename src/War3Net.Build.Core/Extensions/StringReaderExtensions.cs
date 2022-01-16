22af22dd21ed7b0a47be5c853542ef6924588e8f
Drake53 <49623303+Drake53@users.noreply.github.com>
2022-01-16 20:25:26 +0100

    Add extension method to read TriggerData from StringReader.

---
+++ src/War3Net.Build.Core/Extensions/StringReaderExtensions.cs
@@ -0,0 +1,18 @@
+﻿// ------------------------------------------------------------------------------
+// <copyright file="StringReaderExtensions.cs" company="Drake53">
+// Licensed under the MIT license.
+// See the LICENSE file in the project root for more information.
+// </copyright>
+// ------------------------------------------------------------------------------
+
+using System.IO;
+
+using War3Net.Build.Script;
+
+namespace War3Net.Build.Extensions
+{
+    public static class StringReaderExtensions
+    {
+        public static TriggerData ReadTriggerData(this StringReader reader) => new TriggerData(reader);
+    }
+}
