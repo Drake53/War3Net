11adcb28f76f0890d95e092466e35630c2a19421
Drake53 <49623303+Drake53@users.noreply.github.com>
2022-05-26 19:09:48 +0200

    Add GameBuildsProvider.

---
+++ src/War3Net.Build.Core/Providers/GameBuildsProvider.cs
@@ -0,0 +1,37 @@
+﻿// ------------------------------------------------------------------------------
+// <copyright file="GameBuildsProvider.cs" company="Drake53">
+// Licensed under the MIT license.
+// See the LICENSE file in the project root for more information.
+// </copyright>
+// ------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+using War3Net.Build.Common;
+using War3Net.Build.Serialization;
+
+namespace War3Net.Build.Providers
+{
+    public static class GameBuildsProvider
+    {
+        private static readonly Lazy<List<GameBuildVersionInfo>> _builds = new(GetGameBuildsFromJson);
+
+        public static List<GameBuildVersionInfo> GetGameBuilds() => _builds.Value;
+
+        private static List<GameBuildVersionInfo> GetGameBuildsFromJson()
+        {
+            var options = new JsonSerializerOptions(JsonSerializerDefaults.General)
+            {
+                AllowTrailingCommas = true,
+            };
+
+            options.Converters.Add(new JsonStringEnumConverter());
+            options.Converters.Add(new JsonStringVersionConverter());
+
+            return JsonSerializer.Deserialize<List<GameBuildVersionInfo>>(Resources.War3Resources.GameBuilds, options) ?? new();
+        }
+    }
+}
