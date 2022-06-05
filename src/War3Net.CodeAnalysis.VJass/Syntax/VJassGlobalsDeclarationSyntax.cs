0c1c7d6f2df6e294fcebd94011f2fee0d7775590
Drake53 <49623303+Drake53@users.noreply.github.com>
2022-06-05 18:26:33 +0200

    Add globals declaration syntax.

---
+++ src/War3Net.CodeAnalysis.VJass/Syntax/VJassGlobalsDeclarationSyntax.cs
@@ -0,0 +1,29 @@
+﻿// ------------------------------------------------------------------------------
+// <copyright file="VJassGlobalsDeclarationSyntax.cs" company="Drake53">
+// Licensed under the MIT license.
+// See the LICENSE file in the project root for more information.
+// </copyright>
+// ------------------------------------------------------------------------------
+
+using War3Net.CodeAnalysis.Jass.Syntax;
+
+namespace War3Net.CodeAnalysis.VJass.Syntax
+{
+    public class VJassGlobalsDeclarationSyntax : ITopLevelDeclarationSyntax
+    {
+        public VJassGlobalsDeclarationSyntax(VJassGlobalDeclarationListSyntax globals)
+        {
+            Globals = globals;
+        }
+
+        public VJassGlobalDeclarationListSyntax Globals { get; }
+
+        public bool Equals(ITopLevelDeclarationSyntax? other)
+        {
+            return other is VJassGlobalsDeclarationSyntax globalsDeclaration
+                && Globals.Equals(globalsDeclaration.Globals);
+        }
+
+        public override string ToString() => $"{VJassKeyword.Globals} [{Globals.Globals.Length}]";
+    }
+}
---
+++ src/War3Net.CodeAnalysis.VJass/Syntax/VJassScopedGlobalsDeclarationSyntax.cs
@@ -0,0 +1,29 @@
+﻿// ------------------------------------------------------------------------------
+// <copyright file="VJassScopedGlobalsDeclarationSyntax.cs" company="Drake53">
+// Licensed under the MIT license.
+// See the LICENSE file in the project root for more information.
+// </copyright>
+// ------------------------------------------------------------------------------
+
+using War3Net.CodeAnalysis.Jass.Syntax;
+
+namespace War3Net.CodeAnalysis.VJass.Syntax
+{
+    public class VJassScopedGlobalsDeclarationSyntax : IScopedDeclarationSyntax
+    {
+        public VJassScopedGlobalsDeclarationSyntax(VJassScopedGlobalDeclarationListSyntax globals)
+        {
+            Globals = globals;
+        }
+
+        public VJassScopedGlobalDeclarationListSyntax Globals { get; }
+
+        public bool Equals(IScopedDeclarationSyntax? other)
+        {
+            return other is VJassScopedGlobalsDeclarationSyntax scopedGlobalsDeclaration
+                && Globals.Equals(scopedGlobalsDeclaration.Globals);
+        }
+
+        public override string ToString() => $"{VJassKeyword.Globals} [{Globals.Globals.Length}]";
+    }
+}
