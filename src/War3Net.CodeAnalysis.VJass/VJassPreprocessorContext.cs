3489e46ee05c5c82cb21688429501a7ad1d8d32c
Drake53 <49623303+Drake53@users.noreply.github.com>
2022-07-03 16:00:44 +0200

    Add VJassPreprocessorContext.

---
+++ src/War3Net.CodeAnalysis.VJass/VJassPreprocessorContext.cs
@@ -0,0 +1,28 @@
+﻿// ------------------------------------------------------------------------------
+// <copyright file="VJassPreprocessorContext.cs" company="Drake53">
+// Licensed under the MIT license.
+// See the LICENSE file in the project root for more information.
+// </copyright>
+// ------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+
+using War3Net.CodeAnalysis.VJass.Syntax;
+
+namespace War3Net.CodeAnalysis.VJass
+{
+    public class VJassPreprocessorContext
+    {
+        public VJassPreprocessorContext()
+        {
+            KeepNoVJassPreprocessorDirectives = true;
+
+            TextMacros = new Dictionary<string, VJassTextMacroPreprocessorDirectiveTrivia>(StringComparer.Ordinal);
+        }
+
+        public bool KeepNoVJassPreprocessorDirectives { get; }
+
+        internal Dictionary<string, VJassTextMacroPreprocessorDirectiveTrivia> TextMacros { get; }
+    }
+}
