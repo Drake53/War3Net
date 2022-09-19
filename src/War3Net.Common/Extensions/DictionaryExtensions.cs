947e9b3824e682990bd33b5a632b03e0435a41ba
Drake53 <49623303+Drake53@users.noreply.github.com>
2022-09-19 11:32:41 +0200

    Change DictionaryExtensions to extend interface.

--- src/War3Net.Common/Extensions/DictionaryExtensions.cs
+++ src/War3Net.Common/Extensions/DictionaryExtensions.cs
@@ -12,7 +12,7 @@ namespace War3Net.Common.Extensions
 {
     public static class DictionaryExtensions
     {
-        public static void SetValue<TKey, TValue>(this Dictionary<TKey, TValue> dict, TKey key, TValue value)
+        public static void SetValue<TKey, TValue>(this IDictionary<TKey, TValue> dict, TKey key, TValue value)
             where TKey : notnull
         {
             if (dict is null)
