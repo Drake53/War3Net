ed20a6bd5cff82d4434b3046997def4f433a0e9a
Drake53 <49623303+Drake53@users.noreply.github.com>
2022-10-23 13:24:33 +0200

    Add model methods to get nodes.

--- src/War3Net.Modeling/DataStructures/Model.cs
+++ src/War3Net.Modeling/DataStructures/Model.cs
@@ -5,6 +5,9 @@
 // </copyright>
 // ------------------------------------------------------------------------------
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Numerics;
 
 namespace War3Net.Modeling.DataStructures
@@ -50,5 +53,58 @@ namespace War3Net.Modeling.DataStructures
         public RibbonEmitter[]? RibbonEmitters { get; set; }
 
         public TextureAnimation[]? TextureAnimations { get; set; }
+
+        public INode GetNodeById(uint id)
+        {
+            return GetNodes().Single(node => node.ObjectId == id);
+        }
+
+        public IEnumerable<INode> GetNodes()
+        {
+            foreach (var bone in Bones ?? Array.Empty<Bone>())
+            {
+                yield return bone;
+            }
+
+            foreach (var helper in Helpers ?? Array.Empty<Helper>())
+            {
+                yield return helper;
+            }
+
+            foreach (var attachment in Attachments ?? Array.Empty<Attachment>())
+            {
+                yield return attachment;
+            }
+
+            foreach (var eventObject in EventObjects ?? Array.Empty<EventObject>())
+            {
+                yield return eventObject;
+            }
+
+            foreach (var collisionShape in CollisionShapes ?? Array.Empty<CollisionShape>())
+            {
+                yield return collisionShape;
+            }
+
+            foreach (var particleEmitter in ParticleEmitters ?? Array.Empty<ParticleEmitter>())
+            {
+                yield return particleEmitter;
+            }
+
+            foreach (var particleEmitter2 in ParticleEmitters2 ?? Array.Empty<ParticleEmitter2>())
+            {
+                yield return particleEmitter2;
+            }
+
+            foreach (var light in Lights ?? Array.Empty<Light>())
+            {
+                yield return light;
+            }
+
+            foreach (var ribbonEmitter in RibbonEmitters ?? Array.Empty<RibbonEmitter>())
+            {
+                yield return ribbonEmitter;
+            }
+        }
     }
 }
