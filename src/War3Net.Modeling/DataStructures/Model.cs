379dbba730f7133b945eca154c411a2d491316b3
Drake53 <49623303+Drake53@users.noreply.github.com>
2022-10-23 13:23:26 +0200

    Model properties nullable.

--- src/War3Net.Modeling/DataStructures/Model.cs
+++ src/War3Net.Modeling/DataStructures/Model.cs
@@ -15,40 +15,40 @@ namespace War3Net.Modeling.DataStructures
 
         public ModelInfo? ModelInfo { get; set; }
 
-        public Sequence[] Sequences { get; set; }
+        public Sequence[]? Sequences { get; set; }
 
-        public Texture[] Textures { get; set; }
+        public Texture[]? Textures { get; set; }
 
-        public Material[] Materials { get; set; }
+        public Material[]? Materials { get; set; }
 
-        public Geoset[] Geosets { get; set; }
+        public Geoset[]? Geosets { get; set; }
 
-        public GeosetAnimation[] GeosetAnimations { get; set; }
+        public GeosetAnimation[]? GeosetAnimations { get; set; }
 
-        public Bone[] Bones { get; set; }
+        public Bone[]? Bones { get; set; }
 
-        public Helper[] Helpers { get; set; }
+        public Helper[]? Helpers { get; set; }
 
-        public Attachment[] Attachments { get; set; }
+        public Attachment[]? Attachments { get; set; }
 
-        public Vector3[] PivotPoints { get; set; }
+        public Vector3[]? PivotPoints { get; set; }
 
-        public EventObject[] EventObjects { get; set; }
+        public EventObject[]? EventObjects { get; set; }
 
-        public CollisionShape[] CollisionShapes { get; set; }
+        public CollisionShape[]? CollisionShapes { get; set; }
 
-        public GlobalSequence[] GlobalSequences { get; set; }
+        public GlobalSequence[]? GlobalSequences { get; set; }
 
-        public ParticleEmitter[] ParticleEmitters { get; set; }
+        public ParticleEmitter[]? ParticleEmitters { get; set; }
 
-        public ParticleEmitter2[] ParticleEmitters2 { get; set; }
+        public ParticleEmitter2[]? ParticleEmitters2 { get; set; }
 
-        public Camera[] Cameras { get; set; }
+        public Camera[]? Cameras { get; set; }
 
-        public Light[] Lights { get; set; }
+        public Light[]? Lights { get; set; }
 
-        public RibbonEmitter[] RibbonEmitters { get; set; }
+        public RibbonEmitter[]? RibbonEmitters { get; set; }
 
-        public TextureAnimation[] TextureAnimations { get; set; }
+        public TextureAnimation[]? TextureAnimations { get; set; }
     }
 }
