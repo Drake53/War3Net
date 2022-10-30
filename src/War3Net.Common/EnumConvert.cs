5e865a365de9014b3c3142765e22efd240d4f986
Drake53 <49623303+Drake53@users.noreply.github.com>
2022-10-30 10:34:01 +0100

    EnumConvert use Unsafe.As instead of boxing.

--- src/War3Net.Common/EnumConvert.cs
+++ src/War3Net.Common/EnumConvert.cs
@@ -7,6 +7,7 @@
 
 using System;
 using System.Globalization;
+using System.Runtime.CompilerServices;
 
 using War3Net.Common.Extensions;
 
@@ -17,7 +18,12 @@ namespace War3Net.Common
     {
         public static TEnum FromByte(byte value, bool allowNoFlags = true)
         {
-            var result = (TEnum)(object)value;
+            if (Enum.GetUnderlyingType(typeof(TEnum)) != typeof(byte))
+            {
+                throw new InvalidOperationException($"FromByte requires that enum of type {typeof(TEnum).Name} uses byte as its underlying type.");
+            }
+
+            var result = Unsafe.As<byte, TEnum>(ref value);
             if (!result.IsDefined(allowNoFlags))
             {
                 var displayValue = Attribute.GetCustomAttribute(typeof(TEnum), typeof(FlagsAttribute)) is null
@@ -32,7 +38,12 @@ namespace War3Net.Common
 
         public static TEnum FromSByte(sbyte value, bool allowNoFlags = true)
         {
-            var result = (TEnum)(object)value;
+            if (Enum.GetUnderlyingType(typeof(TEnum)) != typeof(sbyte))
+            {
+                throw new InvalidOperationException($"FromSByte requires that enum of type {typeof(TEnum).Name} uses sbyte as its underlying type.");
+            }
+
+            var result = Unsafe.As<sbyte, TEnum>(ref value);
             if (!result.IsDefined(allowNoFlags))
             {
                 var displayValue = Attribute.GetCustomAttribute(typeof(TEnum), typeof(FlagsAttribute)) is null
@@ -47,7 +58,12 @@ namespace War3Net.Common
 
         public static TEnum FromInt16(short value, bool allowNoFlags = true)
         {
-            var result = (TEnum)(object)value;
+            if (Enum.GetUnderlyingType(typeof(TEnum)) != typeof(short))
+            {
+                throw new InvalidOperationException($"FromInt16 requires that enum of type {typeof(TEnum).Name} uses short as its underlying type.");
+            }
+
+            var result = Unsafe.As<short, TEnum>(ref value);
             if (!result.IsDefined(allowNoFlags))
             {
                 var displayValue = Attribute.GetCustomAttribute(typeof(TEnum), typeof(FlagsAttribute)) is null
@@ -62,7 +78,12 @@ namespace War3Net.Common
 
         public static TEnum FromUInt16(ushort value, bool allowNoFlags = true)
         {
-            var result = (TEnum)(object)value;
+            if (Enum.GetUnderlyingType(typeof(TEnum)) != typeof(ushort))
+            {
+                throw new InvalidOperationException($"FromUInt16 requires that enum of type {typeof(TEnum).Name} uses ushort as its underlying type.");
+            }
+
+            var result = Unsafe.As<ushort, TEnum>(ref value);
             if (!result.IsDefined(allowNoFlags))
             {
                 var displayValue = Attribute.GetCustomAttribute(typeof(TEnum), typeof(FlagsAttribute)) is null
@@ -77,7 +98,12 @@ namespace War3Net.Common
 
         public static TEnum FromInt32(int value, bool allowNoFlags = true)
         {
-            var result = (TEnum)(object)value;
+            if (Enum.GetUnderlyingType(typeof(TEnum)) != typeof(int))
+            {
+                throw new InvalidOperationException($"FromInt32 requires that enum of type {typeof(TEnum).Name} uses int as its underlying type.");
+            }
+
+            var result = Unsafe.As<int, TEnum>(ref value);
             if (!result.IsDefined(allowNoFlags))
             {
                 var displayValue = Attribute.GetCustomAttribute(typeof(TEnum), typeof(FlagsAttribute)) is null
@@ -92,7 +118,12 @@ namespace War3Net.Common
 
         public static TEnum FromUInt32(uint value, bool allowNoFlags = true)
         {
-            var result = (TEnum)(object)value;
+            if (Enum.GetUnderlyingType(typeof(TEnum)) != typeof(uint))
+            {
+                throw new InvalidOperationException($"FromUInt32 requires that enum of type {typeof(TEnum).Name} uses uint as its underlying type.");
+            }
+
+            var result = Unsafe.As<uint, TEnum>(ref value);
             if (!result.IsDefined(allowNoFlags))
             {
                 var displayValue = Attribute.GetCustomAttribute(typeof(TEnum), typeof(FlagsAttribute)) is null
@@ -107,7 +138,12 @@ namespace War3Net.Common
 
         public static TEnum FromInt64(long value, bool allowNoFlags = true)
         {
-            var result = (TEnum)(object)value;
+            if (Enum.GetUnderlyingType(typeof(TEnum)) != typeof(long))
+            {
+                throw new InvalidOperationException($"FromInt64 requires that enum of type {typeof(TEnum).Name} uses long as its underlying type.");
+            }
+
+            var result = Unsafe.As<long, TEnum>(ref value);
             if (!result.IsDefined(allowNoFlags))
             {
                 var displayValue = Attribute.GetCustomAttribute(typeof(TEnum), typeof(FlagsAttribute)) is null
@@ -122,7 +158,12 @@ namespace War3Net.Common
 
         public static TEnum FromUInt64(ulong value, bool allowNoFlags = true)
         {
-            var result = (TEnum)(object)value;
+            if (Enum.GetUnderlyingType(typeof(TEnum)) != typeof(ulong))
+            {
+                throw new InvalidOperationException($"FromUInt64 requires that enum of type {typeof(TEnum).Name} uses ulong as its underlying type.");
+            }
+
+            var result = Unsafe.As<ulong, TEnum>(ref value);
             if (!result.IsDefined(allowNoFlags))
             {
                 var displayValue = Attribute.GetCustomAttribute(typeof(TEnum), typeof(FlagsAttribute)) is null
