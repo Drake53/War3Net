8a501a06a49dd08dd951bb9bd7413febe61fb4e5
Drake53 <49623303+Drake53@users.noreply.github.com>
2023-03-17 19:17:46 +0100

    Rename

--- src/War3Net.CodeAnalysis.Jass/SyntaxFactory/GlobalVariableDeclarationFactory.cs
+++ src/War3Net.CodeAnalysis.Jass/SyntaxFactory/GlobalVariableDeclarationFactory.cs
@@ -1,5 +1,5 @@
 ﻿// ------------------------------------------------------------------------------
-// <copyright file="GlobalDeclarationFactory.cs" company="Drake53">
+// <copyright file="GlobalVariableDeclarationFactory.cs" company="Drake53">
 // Licensed under the MIT license.
 // See the LICENSE file in the project root for more information.
 // </copyright>
