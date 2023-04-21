25f7e0c2679dd62c3f73292b8418175854afcf28
Drake53 <49623303+Drake53@users.noreply.github.com>
2023-04-21 19:30:29 +0200

    Add Identifier and IdentifierName methods to JassSyntaxFactory.

---
+++ src/War3Net.CodeAnalysis.Jass/SyntaxFactory/IdentifierNameFactory.cs
@@ -0,0 +1,46 @@
+﻿// ------------------------------------------------------------------------------
+// <copyright file="IdentifierNameFactory.cs" company="Drake53">
+// Licensed under the MIT license.
+// See the LICENSE file in the project root for more information.
+// </copyright>
+// ------------------------------------------------------------------------------
+
+using System;
+
+using War3Net.CodeAnalysis.Jass.Syntax;
+
+namespace War3Net.CodeAnalysis.Jass
+{
+    public static partial class JassSyntaxFactory
+    {
+        public static JassIdentifierNameSyntax IdentifierName(JassSyntaxToken identifier)
+        {
+            if (identifier.SyntaxKind != JassSyntaxKind.IdentifierToken)
+            {
+                throw new ArgumentException("The token's syntax kind must be 'IdentifierToken'.", nameof(identifier));
+            }
+
+            return new JassIdentifierNameSyntax(identifier);
+        }
+
+        public static JassIdentifierNameSyntax IdentifierName(string identifierName)
+        {
+            return new JassIdentifierNameSyntax(Identifier(identifierName));
+        }
+
+        public static JassSyntaxToken Identifier(string text)
+        {
+            return Identifier(JassSyntaxTriviaList.Empty, text, JassSyntaxTriviaList.Empty);
+        }
+
+        public static JassSyntaxToken Identifier(JassSyntaxTriviaList leadingTrivia, string text, JassSyntaxTriviaList trailingTrivia)
+        {
+            if (!JassSyntaxFacts.IsValidIdentifier(text))
+            {
+                throw new ArgumentException($"'{text}' is not a valid identifier.", nameof(text));
+            }
+
+            return new JassSyntaxToken(leadingTrivia, JassSyntaxKind.IdentifierToken, text, trailingTrivia);
+        }
+    }
+}
