baaca637a683672616fe1bac7444369d3fbec156
Drake53 <49623303+Drake53@users.noreply.github.com>
2023-09-30 15:02:46 +0200

    Move MpqStream compression and encryption logic to separate class.

---
+++ src/War3Net.IO.Mpq/MpqStreamUtils.cs
@@ -0,0 +1,160 @@
+﻿// ------------------------------------------------------------------------------
+// <copyright file="MpqStreamUtils.cs" company="Drake53">
+// Licensed under the MIT license.
+// See the LICENSE file in the project root for more information.
+// </copyright>
+// ------------------------------------------------------------------------------
+
+using System;
+using System.IO;
+using System.Text;
+
+using War3Net.Common.Extensions;
+using War3Net.Common.Providers;
+using War3Net.IO.Compression;
+
+namespace War3Net.IO.Mpq
+{
+    internal static class MpqStreamUtils
+    {
+        internal static Stream Compress(Stream baseStream, MpqCompressionType compressionType, int? targetBlockSize)
+        {
+            var resultStream = new MemoryStream();
+            var singleUnit = !targetBlockSize.HasValue;
+
+            void TryCompress(uint bytes)
+            {
+                var offset = baseStream.Position;
+                var compressedStream = compressionType switch
+                {
+                    MpqCompressionType.ZLib => ZLibCompression.Compress(baseStream, (int)bytes, true),
+
+                    _ => throw new NotSupportedException($"Compression type '{compressionType}' is not supported. Currently only ZLib is supported."),
+                };
+
+                // Add one because CompressionType byte not written yet.
+                var length = compressedStream.Length + 1;
+                if (!singleUnit && length >= bytes)
+                {
+                    baseStream.CopyTo(resultStream, offset, (int)bytes, StreamExtensions.DefaultBufferSize);
+                }
+                else
+                {
+                    resultStream.WriteByte((byte)compressionType);
+                    compressedStream.Position = 0;
+                    compressedStream.CopyTo(resultStream);
+                }
+
+                compressedStream.Dispose();
+            }
+
+            var length = (uint)baseStream.Length;
+
+            if (targetBlockSize.HasValue)
+            {
+                var blockCount = (uint)((length + targetBlockSize - 1) / targetBlockSize) + 1;
+                var blockOffsets = new uint[blockCount];
+
+                blockOffsets[0] = 4 * blockCount;
+                resultStream.Position = blockOffsets[0];
+
+                for (var blockIndex = 1; blockIndex < blockCount; blockIndex++)
+                {
+                    var bytesToCompress = blockIndex + 1 == blockCount ? (uint)(baseStream.Length - baseStream.Position) : (uint)targetBlockSize;
+
+                    TryCompress(bytesToCompress);
+                    blockOffsets[blockIndex] = (uint)resultStream.Position;
+                }
+
+                resultStream.Position = 0;
+                using (var writer = new BinaryWriter(resultStream, UTF8EncodingProvider.StrictUTF8, true))
+                {
+                    for (var blockIndex = 0; blockIndex < blockCount; blockIndex++)
+                    {
+                        writer.Write(blockOffsets[blockIndex]);
+                    }
+                }
+            }
+            else
+            {
+                TryCompress(length);
+            }
+
+            resultStream.Position = 0;
+            return resultStream;
+        }
+
+        internal static Stream Encrypt(Stream baseStream, uint encryptionSeed, uint? fileEncryptionOffset, int? targetBlockSize, uint? uncompressedSize)
+        {
+            if (uncompressedSize.HasValue && uncompressedSize.Value < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(uncompressedSize));
+            }
+
+            var compressedSize = (uint)baseStream.Length;
+            var fileSize = uncompressedSize ?? compressedSize;
+
+            var resultStream = new MemoryStream();
+
+            int[] blockPositions;
+            if (targetBlockSize.HasValue)
+            {
+                blockPositions = new int[(uint)(((int)fileSize + targetBlockSize.Value - 1) / targetBlockSize.Value) + 1];
+
+                if (uncompressedSize.HasValue)
+                {
+                    using (var reader = new BinaryReader(baseStream, Encoding.UTF8, true))
+                    {
+                        for (var i = 0; i < blockPositions.Length; i++)
+                        {
+                            blockPositions[i] = (int)reader.ReadUInt32();
+                        }
+                    }
+
+                    baseStream.Seek(0, SeekOrigin.Begin);
+                }
+                else
+                {
+                    for (var i = 0; i + 1 < blockPositions.Length; i++)
+                    {
+                        blockPositions[i] = targetBlockSize.Value * i;
+                    }
+
+                    blockPositions[^1] = (int)compressedSize;
+                }
+            }
+            else
+            {
+                blockPositions = new int[2];
+                blockPositions[0] = 0;
+                blockPositions[^1] = (int)compressedSize;
+            }
+
+            if (fileEncryptionOffset.HasValue)
+            {
+                encryptionSeed = MpqEntry.AdjustEncryptionSeed(encryptionSeed, fileEncryptionOffset.Value, fileSize);
+            }
+
+            var currentOffset = 0;
+            using (var writer = new BinaryWriter(resultStream, UTF8EncodingProvider.StrictUTF8, true))
+            {
+                for (var blockIndex = 0; blockIndex < blockPositions.Length; blockIndex++)
+                {
+                    var toWrite = blockPositions[blockIndex] - currentOffset;
+                    if (toWrite == 0)
+                    {
+                        continue;
+                    }
+
+                    var data = StormBuffer.EncryptStream(baseStream, (uint)(encryptionSeed + blockIndex - 1), currentOffset, toWrite);
+                    writer.Write(data);
+
+                    currentOffset += toWrite;
+                }
+            }
+
+            resultStream.Position = 0;
+            return resultStream;
+        }
+    }
+}
---
+++ tests/War3Net.IO.Mpq.Tests/MpqStreamUtilsTests.cs
@@ -0,0 +1,92 @@
+﻿// ------------------------------------------------------------------------------
+// <copyright file="MpqStreamUtilsTests.cs" company="Drake53">
+// Licensed under the MIT license.
+// See the LICENSE file in the project root for more information.
+// </copyright>
+// ------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using War3Net.TestTools.UnitTesting;
+
+namespace War3Net.IO.Mpq.Tests
+{
+    [TestClass]
+    public class MpqStreamUtilsTests
+    {
+        private const int BlockSize = 0x200 << MpqArchiveCreateOptions.DefaultBlockSize;
+        private const string FileName = "Hello world.png";
+
+        [DataTestMethod]
+        [DynamicData(nameof(GetTestData), DynamicDataSourceType.Method)]
+        public void TestCompressMpqStream(int fileSize)
+        {
+            var input = RandomNumberGenerator.GetBytes(fileSize);
+            using var inputStream = new MemoryStream(input);
+
+            using var compressedStream = MpqStreamUtils.Compress(inputStream, MpqCompressionType.ZLib, BlockSize);
+
+            var mpqEntry = new MpqEntry(null, 0, 0, (uint)compressedStream.Length, (uint)input.Length, MpqFileFlags.Exists | MpqFileFlags.Compressed);
+            using var mpqStream = new MpqStream(mpqEntry, compressedStream, BlockSize);
+
+            StreamAssert.AreEqual(inputStream, mpqStream, true, false);
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(GetTestData), DynamicDataSourceType.Method)]
+        public void TestEncryptMpqStream(int fileSize)
+        {
+            var input = RandomNumberGenerator.GetBytes(fileSize);
+            using var inputStream = new MemoryStream(input);
+
+            var encryptionSeed = MpqEntry.CalculateEncryptionSeed(FileName);
+            using var encryptedStream = MpqStreamUtils.Encrypt(inputStream, encryptionSeed, null, BlockSize, null);
+
+            var mpqEntry = new MpqEntry(FileName, 0, 0, (uint)encryptedStream.Length, (uint)input.Length, MpqFileFlags.Exists | MpqFileFlags.Encrypted);
+            using var mpqStream = new MpqStream(mpqEntry, encryptedStream, BlockSize);
+
+            Assert.IsTrue(mpqStream.CanRead, "Unable to decrypt stream.");
+            Assert.AreEqual(encryptionSeed, mpqEntry.BaseEncryptionSeed);
+            StreamAssert.AreEqual(inputStream, mpqStream, true, false);
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(GetTestData), DynamicDataSourceType.Method)]
+        public void TestCompressAndEncryptMpqStream(int fileSize)
+        {
+            var input = RandomNumberGenerator.GetBytes(fileSize);
+            using var inputStream = new MemoryStream(input);
+
+            using var compressedStream = MpqStreamUtils.Compress(inputStream, MpqCompressionType.ZLib, BlockSize);
+
+            var encryptionSeed = MpqEntry.CalculateEncryptionSeed(FileName);
+            var uncompressedSize = (uint)input.Length;
+            using var encryptedStream = MpqStreamUtils.Encrypt(compressedStream, encryptionSeed, null, BlockSize, uncompressedSize);
+
+            var mpqEntry = new MpqEntry(FileName, 0, 0, (uint)encryptedStream.Length, uncompressedSize, MpqFileFlags.Exists | MpqFileFlags.Compressed | MpqFileFlags.Encrypted);
+            using var mpqStream = new MpqStream(mpqEntry, encryptedStream, BlockSize);
+
+            Assert.IsTrue(mpqStream.CanRead, "Unable to decrypt stream.");
+            Assert.AreEqual(encryptionSeed, mpqEntry.BaseEncryptionSeed);
+            StreamAssert.AreEqual(inputStream, mpqStream, true, false);
+        }
+
+        private static IEnumerable<object?[]> GetTestData()
+        {
+            yield return new object?[] { 0 };
+            yield return new object?[] { 100 };
+            yield return new object?[] { 1000 };
+            yield return new object?[] { 4095 };
+            yield return new object?[] { 4096 };
+            yield return new object?[] { 4097 };
+            yield return new object?[] { 4098 };
+            yield return new object?[] { 4099 };
+            yield return new object?[] { 4100 };
+            yield return new object?[] { 10000 };
+        }
+    }
+}
