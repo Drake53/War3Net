344f1a82e66fb0a9381e2e0b15dae9ce90303ff4
Drake53 <49623303+Drake53@users.noreply.github.com>
2026-01-25 10:29:35 +0100

    Add missing MapScriptBuilder tests.

---
+++ tests/War3Net.Build.Tests/MapScriptBuilder/Info/InitUpgradesTests.cs
@@ -0,0 +1,49 @@
+// ------------------------------------------------------------------------------
+// <copyright file="InitUpgradesTests.cs" company="Drake53">
+// Licensed under the MIT license.
+// See the LICENSE file in the project root for more information.
+// </copyright>
+// ------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using War3Net.TestTools.UnitTesting;
+
+namespace War3Net.Build.Tests
+{
+    public partial class MapScriptBuilderTests
+    {
+        [FlakyTestMethod]
+        [DynamicData(nameof(GetTestDataInitUpgrades), DynamicDataSourceType.Method)]
+        public void TestBodyInitUpgrades(MapScriptBuilderTestData testData)
+        {
+            AssertFunctionGeneratedCorrectly(
+                testData,
+                MapScriptBuilder.GeneratedFunctionName.InitUpgrades,
+                writer => testData.MapScriptBuilder.GenerateInitUpgrades(testData.Map, writer));
+        }
+
+        [FlakyTestMethod]
+        [DynamicData(nameof(GetUnobfuscatedTestData), DynamicDataSourceType.Method)]
+        public void TestConditionInitUpgrades(MapScriptBuilderTestData testData)
+        {
+            var expected = testData.DeclaredFunctions.ContainsKey(MapScriptBuilder.GeneratedFunctionName.InitUpgrades);
+            var actual = testData.MapScriptBuilder.ShouldGenerateInitUpgrades(testData.Map);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        private static IEnumerable<object?[]> GetTestDataInitUpgrades()
+        {
+            foreach (var testData in GetUnobfuscatedTestData())
+            {
+                if (((MapScriptBuilderTestData)testData[0]).DeclaredFunctions.ContainsKey(MapScriptBuilder.GeneratedFunctionName.InitUpgrades))
+                {
+                    yield return testData;
+                }
+            }
+        }
+    }
+}
