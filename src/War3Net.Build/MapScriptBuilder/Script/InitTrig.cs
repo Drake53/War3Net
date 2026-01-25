6f97675a9055f03e24264df0dd8b458b9bcd29ed
Drake53 <49623303+Drake53@users.noreply.github.com>
2026-01-25 08:16:06 +0100

    Delete unused method.

--- src/War3Net.Build/MapScriptBuilder/Script/InitTrig.cs
+++ src/War3Net.Build/MapScriptBuilder/Script/InitTrig.cs
@@ -7,51 +7,12 @@
 
 using System;
 
-using War3Net.Build.Extensions;
 using War3Net.Build.Script;
-using War3Net.CodeAnalysis;
-using War3Net.CodeAnalysis.Jass;
-using War3Net.CodeAnalysis.Jass.Extensions;
 
 namespace War3Net.Build
 {
     public partial class MapScriptBuilder
     {
-        protected internal virtual void GenerateInitTrig(Map map, TriggerDefinition triggerDefinition, IndentedTextWriter writer)
-        {
-            if (map is null)
-            {
-                throw new ArgumentNullException(nameof(map));
-            }
-
-            if (triggerDefinition is null)
-            {
-                throw new ArgumentNullException(nameof(triggerDefinition));
-            }
-
-            if (writer is null)
-            {
-                throw new ArgumentNullException(nameof(writer));
-            }
-
-            var triggerVariableName = triggerDefinition.GetVariableName();
-
-            writer.WriteFunction(triggerDefinition.GetInitTrigFunctionName());
-
-            writer.WriteSet(
-                triggerVariableName,
-                JassExpression.InvokeSpaced(NativeName.CreateTrigger));
-
-            if (!triggerDefinition.IsInitiallyOn)
-            {
-                writer.WriteCall(
-                    NativeName.DisableTrigger,
-                    triggerVariableName);
-            }
-
-            writer.EndFunction();
-        }
-
         protected internal virtual bool ShouldRenderTrigger(Map map, TriggerDefinition triggerDefinition)
         {
             if (map is null)
